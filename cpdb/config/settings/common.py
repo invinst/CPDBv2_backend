652b063c00fe4ae6a6884e0e7913c17173e59e43
ea-open-source <open-source@eastagile.com>
2016-05-20 14:47:07 +0700

    [r] Set the value for ALLOWED_HOSTS temporarily

--- cpdb/config/settings/common.py
+++ cpdb/config/settings/common.py
@@ -15,7 +15,7 @@ environ.Env.read_env("{root}/.env".format(root=ROOT_DIR))  # reading .env file
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # APP CONFIGURATION
 # ------------------------------------------------------------------------------
