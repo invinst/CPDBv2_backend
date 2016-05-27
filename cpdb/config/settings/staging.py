c39f7d3b4569edb432b65aabf3efca869e8e9abe
ea-open-source <open-source@eastagile.com>
2016-05-27 17:19:46 +0700

    [f] 401 - add corsheaders app on staging
    [#120335401] https://www.pivotaltracker.com/story/show/120335401

--- cpdb/config/settings/staging.py
+++ cpdb/config/settings/staging.py
@@ -1,5 +1,9 @@
 from .common import *  # NOQA
 
+INSTALLED_APPS += ('corsheaders',)
+MIDDLEWARE_CLASSES += (
+    'corsheaders.middleware.CorsMiddleware',
+)
 CORS_ORIGIN_WHITELIST = (
     '23.96.180.229:8888',
     )
