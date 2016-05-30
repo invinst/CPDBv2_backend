09a91a9def153a73f1faa2b671dd8fce2181471c
ea-open-source <open-source@eastagile.com>
2016-05-30 10:51:29 +0700

    [f] 401 - remove cors from staging
    [#120335401] https://www.pivotaltracker.com/story/show/120335401

--- cpdb/config/settings/staging.py
+++ cpdb/config/settings/staging.py
@@ -1,11 +1,3 @@
 from .common import *  # NOQA
 
-INSTALLED_APPS += ('corsheaders',)
-MIDDLEWARE_CLASSES += (
-    'corsheaders.middleware.CorsMiddleware',
-)
-CORS_ORIGIN_WHITELIST = (
-    '23.96.180.229:8888',
-    )
-
 DEBUG = False
