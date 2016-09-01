33076166d9ef58fc18095926f25f80c50596a724
ea-open-source <open-source@eastagile.com>
2016-09-01 15:23:35 +0700

    [f] 213 - exclude normal settings from test coverage
    [#129023213] https://www.pivotaltracker.com/story/show/129023213

--- cpdb/manage.py
+++ cpdb/manage.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if 'test' in sys.argv:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.test")
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")  # pragma: no cover
 
     from django.core.management import execute_from_command_line
 
