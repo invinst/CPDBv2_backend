c09230bc59e13209ea7496042cf5fe4851443032
ea-open-source <open-source@eastagile.com>
2016-09-06 10:32:19 +0700

    [f] 995 - Make v2 landing page content editable via wagtail
    [#129595995] https://www.pivotaltracker.com/story/show/129595995

--- cpdb/landing_page/models.py
+++ cpdb/landing_page/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from wagtail.wagtailadmin.edit_handlers import MultiFieldPanel
+from wagtail.wagtailadmin.edit_handlers import MultiFieldPanel, FieldPanel
 from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 from wagtail.wagtailcore.models import Page
 
@@ -15,8 +15,25 @@ class LandingPage(Page):
     faq1 = models.ForeignKey(FAQ, null=True, blank=True, on_delete=models.SET_NULL, related_name='landing_page1')
     faq2 = models.ForeignKey(FAQ, null=True, blank=True, on_delete=models.SET_NULL, related_name='landing_page2')
     faq3 = models.ForeignKey(FAQ, null=True, blank=True, on_delete=models.SET_NULL, related_name='landing_page3')
+    vftg_header = models.CharField(max_length=255, blank=True)
+    vftg_date = models.DateField(blank=True, null=True)
+    vftg_content = models.CharField(max_length=255, blank=True)
+    vftg_link = models.URLField(blank=True)
+    hero_complaints_text = models.CharField(max_length=255, blank=True)
+    hero_use_of_force_text = models.CharField(max_length=255, blank=True)
+
+    # SEO
+    page_title = models.CharField(max_length=255, blank=True)
+    keywords = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True)
 
     content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel('hero_complaints_text'),
+                FieldPanel('hero_use_of_force_text'),
+            ],
+            heading='Hero Section'),
         MultiFieldPanel(
             [
                 SnippetChooserPanel('report1'),
@@ -31,4 +48,18 @@ class LandingPage(Page):
                 SnippetChooserPanel('faq3'),
             ],
             heading='FAQ Section'),
+        MultiFieldPanel(
+            [
+                FieldPanel('vftg_header'),
+                FieldPanel('vftg_date'),
+                FieldPanel('vftg_content'),
+                FieldPanel('vftg_link'),
+            ],
+            heading='VFTG'),
+    ]
+
+    promote_panels = [
+        FieldPanel('page_title'),
+        FieldPanel('keywords'),
+        FieldPanel('description'),
     ]
