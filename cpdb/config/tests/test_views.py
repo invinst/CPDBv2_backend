46e308bcc95316f91e379a8a92245f1997003e34
ea-open-source <open-source@eastagile.com>
2016-09-21 14:10:54 +0700

    [f] 553 - serve base path via django
    [#130722553] https://www.pivotaltracker.com/story/show/130722553

---
+++ cpdb/config/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, RequestFactory
+
+from mock import patch
+
+from config.views import index
+
+
+class ViewsTestCase(TestCase):
+    def test_index_view(self):
+        with patch('config.views.render') as mock_render:
+            request = RequestFactory()
+            index(request)
+            mock_render.assert_called_once_with(request, '/var/www/index.html')
