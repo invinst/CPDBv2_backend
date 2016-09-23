af64b7f317b96100457160dcdd03f3c488ccd48e
ea-open-source <open-source@eastagile.com>
2016-09-23 08:52:58 +0700

    [c] 553 - Ensure csrf cookie is set
    [#130722553] https://www.pivotaltracker.com/story/show/130722553

--- cpdb/config/views.py
+++ cpdb/config/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
+@ensure_csrf_cookie
 def index(request, path=None):
     return render(request, 'index.html')
