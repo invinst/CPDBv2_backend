b9a93ba799f98c9f42b8ee9d7d13c24cdba5d3b1
ea-open-source <open-source@eastagile.com>
2016-10-03 09:13:18 +0700

    [f] 519a - remove unneeded `fields` from deserializer
    [#131144203] https://www.pivotaltracker.com/story/show/131144203

--- cpdb/landing_page/deserializers.py
+++ cpdb/landing_page/deserializers.py
@@ -6,9 +6,6 @@ from landing_page.models import LandingPageContent
 class LandingPageContentDeserializer(serializers.ModelSerializer):
     class Meta:
         model = LandingPageContent
-        fields = (
-            'id', 'collaborate_header', 'collaborate_content'
-            )
 
     def update(self, instance, validated_data):
         instance.collaborate_header = validated_data.get('collaborate_header', instance.collaborate_header)
