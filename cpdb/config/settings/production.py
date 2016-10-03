6495cca02302d9fe8cde43d234bca6f4c10636c2
ea-open-source <open-source@eastagile.com>
2016-10-03 11:53:03 +0700

    [c] add rotate logging on production

--- cpdb/config/settings/production.py
+++ cpdb/config/settings/production.py
@@ -3,11 +3,38 @@ from .common import *  # NOQA
 DEBUG = False
 
 
-INSTALLED_APPS += ('corsheaders',)
-MIDDLEWARE_CLASSES += (
+INSTALLED_APPS += ('corsheaders',)  # NOQA
+MIDDLEWARE_CLASSES += (  # NOQA
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware')
 
 CORS_ORIGIN_WHITELIST = (
     'ms.cpdp.co',
     )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'error-file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/pyenv/versions/cpdb/logs/django-error.log',
+            'maxBytes': 1024*1024*10,  # 10MB
+            'backupCount': 10,
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['error-file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
