f0e0c6d491042d417caaedfadf8e775d66861f19
ea-open-source <open-source@eastagile.com>
2016-10-25 11:01:02 +0700

    [f] 519i - Fix cms view tests
    [#132900473] https://www.pivotaltracker.com/story/show/132900473

--- cpdb/cms/tests/test_views.py
+++ cpdb/cms/tests/test_views.py
@@ -38,7 +38,7 @@ class CMSPageViewSetTestCase(APITestCase):
             }
         ]}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['fields']), 11)
+        self.assertEqual(len(response.data['fields']), 12)
         response_data = {
             field['name']: field for field in response.data['fields']
         }
@@ -55,7 +55,7 @@ class CMSPageViewSetTestCase(APITestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['fields']), 11)
+        self.assertEqual(len(response.data['fields']), 12)
         response_data = {
             field['name']: field for field in response.data['fields']
         }
