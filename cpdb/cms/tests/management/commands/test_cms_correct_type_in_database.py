01cc17dc4b86b0ce83224d151e83b1b3d4809195
ea-open-source <open-source@eastagile.com>
2016-11-16 17:09:49 +0700

    [f] 453 - convert all inline editable text fields to rich text field
    [#134414453] https://www.pivotaltracker.com/story/show/134414453

---
+++ cpdb/cms/tests/management/commands/test_cms_correct_type_in_database.py
@@ -0,0 +1,18 @@
+from django.core.management import call_command
+from django.test import TestCase
+
+from cms.models import ReportPage
+from cms.serializers import ReportPageSerializer
+
+
+class CorrectTypeInDatabaseCommandTestCase(TestCase):
+    def test_call_command(self):
+        report_page_serializer = ReportPageSerializer(data=ReportPageSerializer().fake_data())
+        report_page_serializer.is_valid()
+        report_page_serializer.save()
+        report = ReportPage.objects.first()
+        report.fields['title_type'] = 'plain_text'
+        report.save()
+        call_command('cms_correct_type_in_database')
+        report = ReportPage.objects.first()
+        self.assertEqual(report.fields['title_type'], 'rich_text')
