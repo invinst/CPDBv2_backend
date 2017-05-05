3f58a60f0c0f87460a971a94aa5fefe535042ccb
ea-open-source <open-source@eastagile.com>
2017-05-05 14:34:13 +0700

    [b] 271 - Address no longer show None
    [#144447271] https://www.pivotaltracker.com/story/show/144447271

--- cpdb/data/tests/test_models.py
+++ cpdb/data/tests/test_models.py
@@ -249,6 +249,22 @@ class AllegationTestCase(TestCase):
         allegation = AllegationFactory(add1=3000, add2='Michigan Ave', city='Chicago IL')
         expect(allegation.address).to.eq('3000 Michigan Ave, Chicago IL')
 
+    def test_address_missing_sub_address(self):
+        allegation = AllegationFactory(add1=None, add2='', city='')
+        expect(allegation.address).to.eq('')
+        allegation = AllegationFactory(add1=15, add2='', city='')
+        expect(allegation.address).to.eq('15')
+        allegation = AllegationFactory(add1=None, add2='abc', city='')
+        expect(allegation.address).to.eq('abc')
+        allegation = AllegationFactory(add1=None, add2='', city='Chicago')
+        expect(allegation.address).to.eq('Chicago')
+        allegation = AllegationFactory(add1=15, add2='abc', city='')
+        expect(allegation.address).to.eq('15 abc')
+        allegation = AllegationFactory(add1=15, add2='', city='Chicago')
+        expect(allegation.address).to.eq('15, Chicago')
+        allegation = AllegationFactory(add1=None, add2='abc', city='Chicago')
+        expect(allegation.address).to.eq('abc, Chicago')
+
     def test_officer_allegations(self):
         allegation = AllegationFactory()
         OfficerAllegationFactory(id=1, allegation=allegation, officer=OfficerFactory())
