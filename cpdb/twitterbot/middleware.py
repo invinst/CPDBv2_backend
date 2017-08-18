5daa8e4a6be51f181ddf8bb99991d069f91195d5
ea-open-source <open-source@eastagile.com>
2017-08-18 10:52:45 +0700

    [f] 139 - updated middleware to new syntax
    [#149541139] https://www.pivotaltracker.com/story/show/149541139

--- cpdb/twitterbot/middleware.py
+++ cpdb/twitterbot/middleware.py
@@ -5,11 +5,17 @@ from .models import TwitterBotResponseLog
 logger = logging.getLogger(__name__)
 
 
-class LogTwitterbotLinkVisitMiddleware:
-    def process_request(self, request):
+class LogTwitterbotLinkVisitMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         param = 'twitterbot_log_id'
         if param in request.GET:
             logger.info('%s - Someone visit %s from status %s' % (
                 self.__class__.__name__, request.path,
                 TwitterBotResponseLog.objects.get(id=request.GET[param]).tweet_url))
-        return None
+
+        response = self.get_response(request)
+
+        return response
