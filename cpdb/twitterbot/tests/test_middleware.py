7e722f81e2501fa505d56cc3e36e7ba66af128b2
ea-open-source <open-source@eastagile.com>
2017-08-18 10:52:45 +0700

    [f] 139 - updated middleware to new syntax
    [#149541139] https://www.pivotaltracker.com/story/show/149541139

--- cpdb/twitterbot/tests/test_middleware.py
+++ cpdb/twitterbot/tests/test_middleware.py
@@ -9,17 +9,21 @@ from twitterbot.factories import TwitterBotResponseLogFactory
 
 class LogTwitterbotLinkVisitMiddlewareTestCase(TestCase):
     def test_log_twitterbot_link(self):
-        middleware = LogTwitterbotLinkVisitMiddleware()
+        response = Mock()
+        get_response = Mock(return_value=response)
+        middleware = LogTwitterbotLinkVisitMiddleware(get_response)
         request = Mock(GET={'twitterbot_log_id': 123}, path='/foo/')
         tweet_url = 'http://t.co/status/123/'
         TwitterBotResponseLogFactory(id=123, tweet_url=tweet_url)
         with patch('twitterbot.middleware.logger.info') as info:
-            expect(middleware.process_request(request)).to.be.none()
+            expect(middleware(request)).to.eq(response)
             info.assert_called_with('LogTwitterbotLinkVisitMiddleware - Someone visit /foo/ from status %s' % tweet_url)
 
     def test_no_log(self):
-        middleware = LogTwitterbotLinkVisitMiddleware()
+        response = Mock()
+        get_response = Mock(return_value=response)
+        middleware = LogTwitterbotLinkVisitMiddleware(get_response)
         request = Mock(GET={}, path='/foo/')
         with patch('twitterbot.middleware.logger.info') as info:
-            expect(middleware.process_request(request)).to.be.none()
+            expect(middleware(request)).to.eq(response)
             info.assert_not_called()
