d704d995dc414cd50e8bab63f21d698d38dc93fc
ea-open-source <open-source@eastagile.com>
2017-08-18 09:47:07 +0700

    [f] 139 - updated logging for test settings
    [#149541139] https://www.pivotaltracker.com/story/show/149541139

--- cpdb/config/settings/test.py
+++ cpdb/config/settings/test.py
@@ -1,6 +1,15 @@
 from .common import *  # NOQA
+from .common import APPS_DIR
 
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 MEDIA_ROOT = str(APPS_DIR('test_media'))
 TEST = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {},
+    'handlers': {},
+    'loggers': {}
+}
