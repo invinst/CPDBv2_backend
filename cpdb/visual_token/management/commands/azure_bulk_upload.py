9945f18f17f639cb597cbdccc3ecd40300ce8884
ea-open-source <open-source@eastagile.com>
2017-09-07 16:28:48 +0700

    [f] 598 - Put visual token storage account key into secret files
    [#150702598] https://www.pivotaltracker.com/story/show/150702598

--- cpdb/visual_token/management/commands/azure_bulk_upload.py
+++ cpdb/visual_token/management/commands/azure_bulk_upload.py
@@ -7,7 +7,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Popen([
-            'blobxfer', 'cpdbdev', 'visual-token',
+            'blobxfer', settings.VISUAL_TOKEN_STORAGEACCOUNTNAME, 'visual-token',
             './',
             '--upload', '--include', '*.png', '--no-recursive'],
             cwd=settings.VISUAL_TOKEN_SOCIAL_MEDIA_FOLDER).wait()
