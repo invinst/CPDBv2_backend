5fe4c379f2c63f97d892082f2ed6c161a0c28145
ea-open-source <open-source@eastagile.com>
2017-09-12 15:43:11 +0700

    [c] 243 - create snapshot test for social graph
    [#150964243] https://www.pivotaltracker.com/story/show/150964243

--- cpdb/config/settings/test.py
+++ cpdb/config/settings/test.py
@@ -1,6 +1,14 @@
 from .common import *  # NOQA
+from .common import APPS_DIR, INSTALLED_APPS
 
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 MEDIA_ROOT = str(APPS_DIR('test_media'))
 TEST = True
+
+NOSE_PLUGINS = [
+    'snapshot_test.plugins.CreateSnapshotPlugin',
+    'snapshot_test.plugins.CleanupDirIfSuccessPlugin'
+]
+
+INSTALLED_APPS += ('snapshot_test',)
