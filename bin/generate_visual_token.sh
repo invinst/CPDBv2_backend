ef9b9a8b0c9271ec5321b24e373afff1bd0ff3ae
ea-open-source <open-source@eastagile.com>
2017-09-18 15:11:38 +0700

    [c] 598 - Use xargs to avoid “Arguments list too long” error
    [#150702598] https://www.pivotaltracker.com/story/show/150702598

--- bin/generate_visual_token.sh
+++ bin/generate_visual_token.sh
@@ -6,9 +6,11 @@ source /pyenv/versions/cpdb/bin/activate
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/..
-
 cpdb/manage.py generate_visual_tokens
-optipng $VISUAL_TOKEN_MEDIA_PATH*.png
+
+cd $VISUAL_TOKEN_MEDIA_PATH
+find -name '*.png' -print0 | xargs -0 optipng
+
 azcopy \
   --quiet \
   --source $VISUAL_TOKEN_MEDIA_PATH \
