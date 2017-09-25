655d374e7452d257a33d4b8abfb1cd076ab9903a
ea-open-source <open-source@eastagile.com>
2017-09-25 11:37:32 +0700

    [f] 540 - Use Google Natural Language API to extract name in TwitterBot
    [#151280540] https://www.pivotaltracker.com/story/show/151280540

--- cpdb/twitterbot/name_parsers.py
+++ cpdb/twitterbot/name_parsers.py
@@ -1,24 +1,18 @@
-import logging
-
 from django.conf import settings
-from rosette.api import API, DocumentParameters, RosetteException
 
-logger = logging.getLogger(__name__)
+from google.cloud.language import enums, types, LanguageServiceClient
 
 
-class RosettePersonNameParser:
+class GoogleNaturalLanguageNameParser:
     def __init__(self):
-        self.api = API(user_key=settings.ROSETTE_API_KEY)
-        self.api.setOption('linkEntities', False)
-        self.type = 'PERSON'
+        if not settings.TEST:
+            self.client = LanguageServiceClient()  # pragma: no cover
+        self.type = enums.Entity.Type.PERSON
 
     def parse(self, content):
         source, text = content
-        params = DocumentParameters()
-        params['content'] = text
-        try:
-            entities = self.api.entities(params)
-        except RosetteException, exception:
-            logger.error('Error while parsing "%s": %s' % (text, str(exception)))
-            return []
-        return [(source, e['mention']) for e in entities['entities'] if e['type'] == self.type]
+        document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT)
+        entities = self.client.analyze_entities(document=document).entities
+        return [(source, e.name) for e in entities if e.type == self.type]
