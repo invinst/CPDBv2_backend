626587853089dfe0c8010bcf3d6ae008fc0f2c64
ea-open-source <open-source@eastagile.com>
2017-11-27 11:24:06 +0700

    [b] 900 - Add necessary fields to UnitOfficerIndexer to avoid error 500
    [#153114900] https://www.pivotaltracker.com/story/show/153114900

--- cpdb/search/tests/test_indexers.py
+++ cpdb/search/tests/test_indexers.py
@@ -213,7 +213,7 @@ class CommunityIndexerTestCase(TestCase):
 class UnitOfficerIndexerTestCase(TestCase):
     def setUp(self):
         unit = PoliceUnitFactory(unit_name='001')
-        officer = OfficerFactory(first_name='Kevin', last_name='Osborn')
+        officer = OfficerFactory(first_name='Kevin', last_name='Osborn', rank='somebody', race='White', gender='M')
         self.history = OfficerHistoryFactory(unit=unit, officer=officer)
 
     def test_get_queryset(self):
@@ -225,7 +225,12 @@ class UnitOfficerIndexerTestCase(TestCase):
             'badge': '',
             'to': self.history.officer.v2_to,
             'allegation_count': 0,
-            'unit_name': '001'
+            'unit_name': '001',
+            'unit': '001',
+            'rank': 'somebody',
+            'race': 'White',
+            'sex': 'Male',
+            'visual_token_background_color': '#f5f4f4'
         })
 
 
