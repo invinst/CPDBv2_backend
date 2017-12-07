38dddd27b2c966eb88c76afd939ba30dcbb9ccaf
ea-open-source <open-source@eastagile.com>
2017-12-07 11:46:09 +0700

    [f] 459 - Use bulk index to decrease Elasticsearch index time
    [#153351459] https://www.pivotaltracker.com/story/show/153351459

--- cpdb/twitterbot/tests/mixins.py
+++ cpdb/twitterbot/tests/mixins.py
@@ -4,10 +4,10 @@ from twitterbot.indexers import OfficerIndexer
 
 class RebuildIndexMixin:
     def setUp(self):
-        twitterbot_index_alias._write_index.delete(ignore=404)
-        twitterbot_index_alias._read_index.create(ignore=400)
+        twitterbot_index_alias.write_index.delete(ignore=404)
+        twitterbot_index_alias.read_index.create(ignore=400)
 
     def refresh_index(self):
         with twitterbot_index_alias.indexing():
             OfficerIndexer().reindex()
-        twitterbot_index_alias._write_index.refresh()
+        twitterbot_index_alias.write_index.refresh()
