f08ce5a37884d607eeeb6de39c6286fdf8b28b60
ea-open-source <open-source@eastagile.com>
2017-12-14 14:42:06 +0700

    [f] 493 - Upload neighborhood info to azure blob storage
    [#153621493] https://www.pivotaltracker.com/story/show/153621493

--- cpdb/heatmap/management/commands/upload_heatmap_geojson.py
+++ cpdb/heatmap/management/commands/upload_heatmap_geojson.py
@@ -5,10 +5,15 @@ from tempfile import NamedTemporaryFile
 from django.core.management import BaseCommand
 from django.conf import settings
 from django.db import connection
+from django.db.models import Count
 from django.contrib.gis.geos.factory import fromstr
 
+from tqdm import tqdm
 from azure.storage.blob import BlockBlobService, PublicAccess, ContentSettings
 from azure.storage.common.models import CorsRule
+
+from data.models import Area, Allegation
+from data.constants import NEIGHBORHOODS_AREA_CHOICE, DISCIPLINE_CODES, OUTCOMES_DICT
 
 
 # pragma: no cover
@@ -55,6 +60,55 @@ class Command(BaseCommand):
 
         return json.dumps(ret)
 
+    def get_neighborhood_data(self):
+        areas = Area.objects.filter(area_type=NEIGHBORHOODS_AREA_CHOICE, polygon__isnull=False)
+
+        area_dict = {
+            "type": "FeatureCollection",
+            "features": [],
+        }
+        for area in tqdm(areas):
+            polygon = json.loads(area.polygon.geojson)
+
+            most_common_complaint = Allegation.objects.filter(
+                    areas__id=area.id,
+                    officerallegation__allegation_category__category__isnull=False)\
+                .distinct()\
+                .values('officerallegation__allegation_category__category')\
+                .annotate(category_count=Count('id'))\
+                .order_by('-category_count').first()
+            most_common_discipline = Allegation.objects.filter(
+                    areas__id=area.id,
+                    officerallegation__final_outcome__in=DISCIPLINE_CODES)\
+                .distinct()\
+                .values('officerallegation__final_outcome')\
+                .annotate(outcome_count=Count('id'))\
+                .order_by('-outcome_count').first()
+
+            area_json = {
+                "type": "Feature",
+                "properties": {
+                    'id': area.id,
+                    'name': area.name,
+                    'complaints_count': Allegation.objects.filter(areas__id=area.id).distinct().count(),
+                    'sustaineds_count': Allegation.objects.filter(
+                        areas__id=area.id,
+                        officerallegation__final_finding='SU').distinct().count(),
+                    'most_common_complaint':
+                        most_common_complaint.get('officerallegation__allegation_category__category')
+                        if most_common_complaint
+                        else 'N/A',
+                    'most_common_discipline':
+                        OUTCOMES_DICT.get(most_common_discipline.get('officerallegation__final_outcome'))
+                        if most_common_discipline
+                        else 'N/A'
+                },
+                'geometry': polygon
+            }
+
+            area_dict['features'].append(area_json)
+        return json.dumps(area_dict)
+
     def save_to_gzip_file(self, content):
         tmp_file = NamedTemporaryFile(delete=False)
         with gzip.open(tmp_file.name, 'wb') as f:
@@ -62,13 +116,22 @@ class Command(BaseCommand):
         return tmp_file.name
 
     def handle(self, *args, **options):
+        content_settings = ContentSettings(content_type='application/json', content_encoding='gzip')
+
         block_blob_service = BlockBlobService(
             account_name=settings.AZURE_STORAGE_ACCOUNT_NAME, account_key=settings.AZURE_STORAGE_ACCOUNT_KEY)
         block_blob_service.create_container('heatmap', public_access=PublicAccess.Blob)
         block_blob_service.set_blob_service_properties(cors=[CorsRule(['*'], ['GET'])])
+
         block_blob_service.create_blob_from_path(
             'heatmap',
             'cluster.geojson',
             file_path=self.save_to_gzip_file(self.get_heatmap_cluster_data()),
-            content_settings=ContentSettings(content_type='application/json', content_encoding='gzip')
+            content_settings=content_settings
+        )
+        block_blob_service.create_blob_from_path(
+            'heatmap',
+            'neighborhood.geojson',
+            file_path=self.save_to_gzip_file(self.get_neighborhood_data()),
+            content_settings=content_settings
         )
--- cpdb/heatmap/tests/management/commands/test_upload_heatmap_geojson_command.py
+++ cpdb/heatmap/tests/management/commands/test_upload_heatmap_geojson_command.py
@@ -2,19 +2,46 @@ import gzip
 import json
 
 from django.test import TestCase
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Point, MultiPolygon, Polygon
 from django.core.management import call_command
 
 from robber import expect
 from mock import patch
 
-from data.factories import AllegationFactory
+from data.factories import AllegationFactory, AreaFactory, OfficerAllegationFactory, AllegationCategoryFactory
+from data.constants import NEIGHBORHOODS_AREA_CHOICE
 from heatmap.management.commands.upload_heatmap_geojson import Command
 
 
 class UploadHeatmapGeoJSONCommandTestCase(TestCase):
     def setUp(self):
-        AllegationFactory(point=Point([20, 22]))
+        area = AreaFactory(id=123, name='Hyde Park', area_type=NEIGHBORHOODS_AREA_CHOICE, polygon=MultiPolygon(Polygon((
+            (87.940101, 42.023135),
+            (87.523661, 42.023135),
+            (87.523661, 41.644286),
+            (87.940101, 41.644286),
+            (87.940101, 42.023135))))
+        )
+        AreaFactory(id=124, name='Lincoln Square', area_type=NEIGHBORHOODS_AREA_CHOICE, polygon=MultiPolygon(Polygon((
+            (81, 41),
+            (82, 41),
+            (82, 42),
+            (81, 42),
+            (81, 41))))
+        )
+        allegation1 = AllegationFactory(point=Point([20, 22]))
+        allegation2 = AllegationFactory(point=Point([23, 22]))
+        area.allegation_set.add(allegation1, allegation2)
+        allegation_category = AllegationCategoryFactory(category='Operation/Personnel Violations')
+        OfficerAllegationFactory(
+            allegation=allegation1,
+            allegation_category=allegation_category,
+            final_outcome='100')
+        OfficerAllegationFactory(
+            allegation=allegation2,
+            allegation_category=allegation_category,
+            final_finding='SU',
+            final_outcome='100')
         self.command = Command()
 
     def test_get_heatmap_cluster_data(self):
@@ -29,6 +56,15 @@ class UploadHeatmapGeoJSONCommandTestCase(TestCase):
                 'properties': {
                     'weight': 1
                 }
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'coordinates': [23.0, 22.0],
+                    'type': 'Point'
+                },
+                'properties': {
+                    'weight': 1
+                }
             }]
         }))
 
@@ -37,6 +73,52 @@ class UploadHeatmapGeoJSONCommandTestCase(TestCase):
         file_name = self.command.save_to_gzip_file(expected_content)
         with gzip.open(file_name) as f:
             expect(f.read()).to.eq(expected_content)
+
+    def test_get_neighborhood_data(self):
+        expect(self.command.get_neighborhood_data()).to.eq(json.dumps({
+            'type': 'FeatureCollection',
+            'features': [{
+                'type': 'Feature',
+                'properties': {
+                    'id': 123,
+                    'name': 'Hyde Park',
+                    'complaints_count': 2,
+                    'sustaineds_count': 1,
+                    'most_common_complaint': 'Operation/Personnel Violations',
+                    'most_common_discipline': 'Reprimand'
+                },
+                'geometry': {
+                    'type': 'MultiPolygon',
+                    'coordinates': [[[
+                        [87.940101, 42.023135],
+                        [87.523661, 42.023135],
+                        [87.523661, 41.644286],
+                        [87.940101, 41.644286],
+                        [87.940101, 42.023135]
+                    ]]]
+                }
+            }, {
+                'type': 'Feature',
+                'properties': {
+                    'id': 124,
+                    'name': 'Lincoln Square',
+                    'complaints_count': 0,
+                    'sustaineds_count': 0,
+                    'most_common_complaint': 'N/A',
+                    'most_common_discipline': 'N/A'
+                },
+                'geometry': {
+                    'type': 'MultiPolygon',
+                    'coordinates': [[[
+                        [81.0, 41.0],
+                        [82.0, 41.0],
+                        [82.0, 42.0],
+                        [81.0, 42.0],
+                        [81.0, 41.0]
+                    ]]]
+                }
+            }]
+        }))
 
     @patch('heatmap.management.commands.upload_heatmap_geojson.BlockBlobService', autospec=True)
     def test_handle(self, mock_block_blob_service):
