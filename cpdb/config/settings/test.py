e396177be3ff7121cce6c0360ae3a1842abcd15e
ea-open-source <open-source@eastagile.com>
2018-01-04 10:33:37 +0700

    [r] - Disable migrations during test running

--- cpdb/config/settings/test.py
+++ cpdb/config/settings/test.py
@@ -21,3 +21,14 @@ LOGGING = {
     'handlers': {},
     'loggers': {}
 }
+
+
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
