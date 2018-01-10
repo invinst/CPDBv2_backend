9acc73382dfa2018408347349e8966ea17689efc
ea-open-source <open-source@eastagile.com>
2018-01-10 16:00:50 +0700

    [r] - rename migration

--- cpdb/data/migrations/0027_add_race_population.py
+++ cpdb/data/migrations/0027_add_race_population.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data', '0024_add_investigatorallegation'),
+        ('data', '0026_add_investigatorallegation'),
     ]
 
     operations = [
