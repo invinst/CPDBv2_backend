b1cbd2ca05710319a221d0060c97cdb6504f26d6
ea-open-source <open-source@eastagile.com>
2018-01-24 10:13:53 +0700

    [f] 054 - Created the TRR model
    [#154551054] https://www.pivotaltracker.com/story/show/154551054

---
+++ cpdb/trr/models.py
@@ -0,0 +1,100 @@
+from django.contrib.gis.db import models
+
+from data.constants import GENDER
+
+
+DIRECTION_WEST = 'West'
+DIRECTION_NORTH = 'North'
+DIRECTION_SOUTH = 'South'
+DIRECTION_EAST = 'East'
+DIRECTION_CHOICES = (
+    (DIRECTION_WEST, 'West'),
+    (DIRECTION_NORTH, 'North'),
+    (DIRECTION_SOUTH, 'South'),
+    (DIRECTION_EAST, 'East')
+)
+
+INDOOR = 'Indoor'
+OUTDOOR = 'Outdoor'
+INDOOR_OUTDOOR_CHOICES = (
+    (INDOOR, 'Indoor'),
+    (OUTDOOR, 'Outdoor')
+)
+
+LC_DAYLIGHT = 'DAYLIGHT'
+LC_GOOD_ARTIFICIAL = 'GOOD ARTIFICIAL'
+LC_DUSK = 'DUSK'
+LC_NIGHT = 'NIGHT'
+LC_POOR_ARTIFICIAL = 'POOR ARTIFICIAL'
+LC_DAWN = 'DAWN'
+
+LIGHTNING_CONDITION_CHOICES = (
+    (LC_DAYLIGHT, 'Daylight'),
+    (LC_GOOD_ARTIFICIAL, 'Good Artificial'),
+    (LC_DUSK, 'Dusk'),
+    (LC_NIGHT, 'Night'),
+    (LC_POOR_ARTIFICIAL, 'Poor Artificial'),
+    (LC_DAWN, 'Dawn')
+)
+
+WC_OTHER = 'OTHER'
+WC_CLEAR = 'CLEAR'
+WC_SNOW = 'SNOW'
+WC_RAIN = 'RAIN'
+WC_SLEET_HAIL = 'SLEET/HAIL'
+WC_SEVERE_CROSS_WIND = 'SEVERE CROSS WIND'
+WC_FOG_SMOKE_HAZE = 'FOG/SMOKE/HAZE'
+
+WEATHER_CONDITION_CHOICES = (
+    (WC_OTHER, 'Other')
+    (WC_CLEAR, 'Clear')
+    (WC_SNOW, 'Snow')
+    (WC_RAIN, 'Rain')
+    (WC_SLEET_HAIL, 'Sleet/hail')
+    (WC_SEVERE_CROSS_WIND, 'Severe Cross Wind')
+    (WC_FOG_SMOKE_HAZE, 'Fog/smoke/haze')
+)
+
+PFF_MEMBER = 'MEMBER'
+PFF_OTHER = 'OTHER'
+PFF_OFFENDER = 'OFFENDER'
+
+PARTY_FIRED_FIRST_CHOICES = (
+    (PFF_MEMBER, 'Member'),
+    (PFF_OTHER, 'Other'),
+    (PFF_OFFENDER, 'Offender')
+)
+
+
+class TRR(models.Model):
+    rd_no = models.CharField(max_length=255, null=True)
+    event_no = models.CharField(max_length=255, null=True)
+    beat = models.CharField(max_length=255, null=True)
+    block = models.CharField(max_length=255, null=True)
+    direction = models.CharField(max_length=255, null=True, choices=DIRECTION_CHOICES)
+    street = models.CharField(max_length=255, null=True)
+    location = models.CharField(max_length=255, null=True)
+    trr_datetime = models.DateTimeField()
+    indoor_or_outdoor = models.CharField(max_length=255, null=True, choices=INDOOR_OUTDOOR_CHOICES)
+    lighting_condition = models.CharField(max_length=255, null=True, choices=LIGHTNING_CONDITION_CHOICES)
+    weather_condition = models.CharField(max_length=255, null=True, choices=WEATHER_CONDITION_CHOICES)
+    notify_OEMC = models.NullBooleanField()
+    notify_district_sergeant = models.NullBooleanField()
+    notify_OP_command = models.NullBooleanField()
+    notify_DET_division = models.NullBooleanField()
+    number_of_weapons_discharged = models.PositiveSmallIntegerField()
+    party_fired_first = models.CharField(max_length=255, null=True, choices=PARTY_FIRED_FIRST_CHOICES)
+    officer = models.ForeignKey('data.Officer')
+    officer_assigned_beat = models.CharField(max_length=255, null=True)
+    officer_duty_status = models.NullBooleanField()
+    officer_in_uniform = models.NullBooleanField()
+    officer_injured = models.NullBooleanField()
+    unit = models.ForeignKey('data.PoliceUnit')
+    unit_detail = models.CharField(max_length=255, null=True)
+    subject_gender = models.CharField(max_length=255, null=True, choices=GENDER)
+    subject_race = models.CharField(max_length=255, null=True)
+    subject_age = models.PositiveSmallIntegerField()
+    subject_birth_year = models.CharField(max_length=255, null=True)
+    subject_armed = models.NullBooleanField()
+    subject_injured = models.NullBooleanField()
+    subject_alleged_injury = models.NullBooleanField()
