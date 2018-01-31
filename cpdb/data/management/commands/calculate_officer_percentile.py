18f8a3e5497f48222297e001e0999eadb6e71aad
ea-open-source <open-source@eastagile.com>
2018-02-01 02:50:39 +0700

    [c] 197 - merge with develop and cover test

--- cpdb/data/management/commands/calculate_officer_percentile.py
+++ cpdb/data/management/commands/calculate_officer_percentile.py
@@ -1,20 +1,21 @@
 import time
+import logging
 from django.core.management import BaseCommand
 
 from data.models import Officer
 
+logger = logging.getLogger('django.command')
+
 
 class Command(BaseCommand):
-
     def update_percentile_to_db(self, percentile_values):
-        if not percentile_values:
-            return
-        Officer.objects.all().update(complaint_percentile=None)
-        for officer_id, score in percentile_values:
-            officer = Officer.objects.get(pk=officer_id)
-            if officer:
-                officer.complaint_percentile = score
-                officer.save()
+        if percentile_values:
+            Officer.objects.all().update(complaint_percentile=None)
+            for officer_id, score in percentile_values:
+                officer = Officer.objects.get(pk=officer_id)
+                if officer:
+                    officer.complaint_percentile = score
+                    officer.save()
 
     def handle(self, *args, **kwargs):
         start_time = time.time()
@@ -22,4 +23,4 @@ class Command(BaseCommand):
         top_percentile = Officer.top_complaint_officers(100)  # calculate all
         self.update_percentile_to_db(top_percentile)
 
-        print("--- %s seconds ---" % (time.time() - start_time))
+        logger.info("Finished on --- %s seconds ---" % (time.time() - start_time))
