8e95db8938e677d9034945d256e9f13874eb0bd2
ea-open-source <open-source@eastagile.com>
2018-03-21 18:43:53 +0700

    [f] 449 - extend rebuild_index to rebuild specific type w/o losing data

--- cpdb/es_index/management/commands/rebuild_index.py
+++ cpdb/es_index/management/commands/rebuild_index.py
@@ -1,7 +1,7 @@
 from django.core.management import BaseCommand
 from django.utils.module_loading import autodiscover_modules
 
-from es_index import indexer_klasses, indexer_klasses_map
+from es_index import indexer_klasses, indexer_klasses_map, es_client
 
 
 class Command(BaseCommand):
@@ -14,7 +14,13 @@ class Command(BaseCommand):
         indexers = []
         if len(options['app']):
             for app in options['app']:
-                indexers = indexers + list(indexer_klasses_map[app])
+                app_split = app.split('.')
+                if len(app_split) == 1:
+                    indexers = indexers + list(indexer_klasses_map[app])
+                else:
+                    for indexer in indexer_klasses_map[app_split[0]]:
+                        if indexer.doc_type_klass._doc_type.name == app_split[1]:
+                            indexers.append(indexer)
         else:
             indexers = indexer_klasses
         return indexers
@@ -25,13 +31,41 @@ class Command(BaseCommand):
         for indexer in indexers:
             indexers_map.setdefault(indexer.index_alias.name, []).append(indexer)
             alias_map[indexer.index_alias.name] = indexer.index_alias
-        return [(alias_map[key], indexers_map[key]) for key in alias_map.keys()]
+
+        return [(
+            alias_map[key],
+            indexers_map[key],
+            list(indexer_klasses_map[key] - set(indexers_map[key]))
+        ) for key in alias_map.keys()]
+
+    def _get_distinct_doc_types_from_indexers(self, indexers):
+        return list(set(x.doc_type_klass._doc_type.name for x in indexers))
+
+    def _migrate(self, index_alias, migrate_doc_types=[]):
+        if not migrate_doc_types:
+            return
+
+        index_alias.write_index.open()
+        query = {}
+        query['source'] = {'index': index_alias.name, 'type': migrate_doc_types}
+        query['dest'] = {
+            'index': index_alias.new_index_name,
+            'version_type': 'external'
+        }
+        es_client.reindex(query)
 
     def handle(self, *args, **options):
         selected_indexers = self.get_indexers(**options)
         alias_indexers_tuple = self.categorize_indexers_by_index_alias(selected_indexers)
 
-        for alias, indexers in alias_indexers_tuple:
+        for alias, indexers, migrating_indexers in alias_indexers_tuple:
             with alias.indexing():
-                for indexer_klass in indexers:
-                    indexer_klass().reindex()
+
+                list_migrate_doc_types = self._get_distinct_doc_types_from_indexers(migrating_indexers)
+                indexer_klass_instances = [klass() for klass in indexers]
+                for indexer_instance in indexer_klass_instances:
+                    indexer_instance.create_mapping()
+                self._migrate(alias, list_migrate_doc_types)
+
+                for indexer_instance in indexer_klass_instances:
+                    indexer_instance.add_new_data()
--- cpdb/es_index/tests/commands/test_rebuild_index.py
+++ cpdb/es_index/tests/commands/test_rebuild_index.py
@@ -7,53 +7,68 @@ from es_index import indexer_klasses, indexer_klasses_map
 
 
 class RebuildIndexCommandTestCase(SimpleTestCase):
-    def test_handle(self):
+    def _prepare_data(self):
         class Indexer:
-            index_alias = Mock()
-        Indexer.reindex = Mock()
+            index_alias = Mock(new_index_name='new_name')
+            doc_type_klass = Mock(_doc_type=Mock())
+
+        Indexer.doc_type_klass._doc_type.name = 'a'
+        Indexer.create_mapping = Mock()
+        Indexer.add_new_data = Mock()
+        Indexer.index_alias.name = 'test'
         Indexer.index_alias.indexing.return_value.__exit__ = Mock()
         Indexer.index_alias.indexing.return_value.__enter__ = Mock()
-
+        indexer_klasses_map.setdefault('test', set()).clear()
+        indexer_klasses_map['test'].add(Indexer)
         indexer_klasses.clear()
         indexer_klasses.add(Indexer)
+        return Indexer
+
+    def test_handle(self):
+        Indexer = self._prepare_data()
 
         with patch('es_index.management.commands.rebuild_index.autodiscover_modules'):
             call_command('rebuild_index')
 
-        Indexer.reindex.assert_called_once()
+        Indexer.create_mapping.assert_called_once()
+        Indexer.add_new_data.assert_called_once()
         Indexer.index_alias.indexing.return_value.__exit__.assert_called_once()
         Indexer.index_alias.indexing.return_value.__enter__.assert_called_once()
 
     def test_call_with_app_specified(self):
-        class Indexer:
-            index_alias = Mock()
-        Indexer.reindex = Mock()
-        Indexer.index_alias.indexing.return_value.__exit__ = Mock()
-        Indexer.index_alias.indexing.return_value.__enter__ = Mock()
-
-        indexer_klasses_map.setdefault('test', set()).clear()
-        indexer_klasses_map['test'].add(Indexer)
+        Indexer = self._prepare_data()
 
         with patch('es_index.management.commands.rebuild_index.autodiscover_modules'):
             call_command('rebuild_index', 'test')
 
-        Indexer.reindex.assert_called_once()
+        Indexer.create_mapping.assert_called_once()
+        Indexer.add_new_data.assert_called_once()
         Indexer.index_alias.indexing.return_value.__exit__.assert_called_once()
         Indexer.index_alias.indexing.return_value.__enter__.assert_called_once()
 
     def test_call_alias_indexing_only_once_for_multiple_indexers(self):
         alias = Mock()
+        alias.name = 'alias'
         alias.indexing.return_value.__exit__ = Mock()
         alias.indexing.return_value.__enter__ = Mock()
 
         class Indexer1:
             index_alias = alias
-        Indexer1.reindex = Mock()
+            doc_type_klass = Mock(_doc_type=Mock())
+
+        Indexer1.create_mapping = Mock()
+        Indexer1.add_new_data = Mock()
 
         class Indexer2:
             index_alias = alias
-        Indexer2.reindex = Mock()
+            doc_type_klass = Mock(_doc_type=Mock(name='b'))
 
+        Indexer2.create_mapping = Mock()
+        Indexer2.add_new_data = Mock()
+
+        indexer_klasses_map.setdefault('alias', set()).clear()
+        indexer_klasses_map['alias'].add(Indexer1)
+        indexer_klasses_map['alias'].add(Indexer2)
         indexer_klasses.clear()
         indexer_klasses.add(Indexer1)
         indexer_klasses.add(Indexer2)
@@ -61,7 +76,35 @@ class RebuildIndexCommandTestCase(SimpleTestCase):
         with patch('es_index.management.commands.rebuild_index.autodiscover_modules'):
             call_command('rebuild_index')
 
-        Indexer1.reindex.assert_called_once()
-        Indexer2.reindex.assert_called_once()
+        Indexer1.create_mapping.assert_called_once()
+        Indexer2.create_mapping.assert_called_once()
+        Indexer1.add_new_data.assert_called_once()
+        Indexer2.add_new_data.assert_called_once()
         alias.indexing.return_value.__exit__.assert_called_once()
         alias.indexing.return_value.__enter__.assert_called_once()
+
+    def test_call_alias_indexing_with_specific_doc_type(self):
+        Indexer1 = self._prepare_data()
+
+        class Indexer2:
+            index_alias = Mock(new_index_name='new_name')
+            doc_type_klass = Mock(_doc_type=Mock())
+
+        Indexer2.doc_type_klass._doc_type.name = 'b'
+        Indexer2.index_alias.name = 'test'
+        Indexer2.create_mapping = Mock()
+        Indexer2.add_new_data = Mock()
+        indexer_klasses_map['test'].add(Indexer2)
+        indexer_klasses.add(Indexer2)
+
+        with patch('es_index.es_client.reindex') as mock_reindex:
+            with patch('es_index.management.commands.rebuild_index.autodiscover_modules'):
+                call_command('rebuild_index', 'test.a')
+
+            mock_reindex.assert_called_once()
+
+        Indexer1.create_mapping.assert_called_once()
+        Indexer1.add_new_data.assert_called_once()
+        Indexer2.add_new_data.assert_not_called()
+        Indexer1.index_alias.indexing.return_value.__exit__.assert_called_once()
+        Indexer1.index_alias.indexing.return_value.__enter__.assert_called_once()
