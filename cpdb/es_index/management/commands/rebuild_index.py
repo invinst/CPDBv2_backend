6115beea1c91738d72ada80008b5c926dddea0d6
ea-open-source <open-source@eastagile.com>
2018-03-23 18:16:35 +0700

    [c] - change rebuild_index step when deploy, need config json on azure

--- cpdb/es_index/management/commands/rebuild_index.py
+++ cpdb/es_index/management/commands/rebuild_index.py
@@ -1,28 +1,76 @@
+import json
+
+from django.conf import settings
 from django.core.management import BaseCommand
 from django.utils.module_loading import autodiscover_modules
 
-from es_index import indexer_klasses, indexer_klasses_map, es_client
+from azure.storage.blob import BlockBlobService
+
+from es_index import indexer_klasses, indexer_klasses_map
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('app', nargs='*')
+        parser.add_argument(
+            '--from-file',
+            dest='from_file',
+            help='Read config json and choose which indexer to rebuild'
+        )
+        parser.add_argument(
+            '--from-azure',
+            action='store_true',
+            dest='from_azure',
+            help='Read config json from Azure'
+        )
+
+    def _get_indexer_names_from_json(self, file_name):
+        with open(file_name) as f:
+            return json.load(f)
+
+    def _get_indexer_names_from_args(self, apps):
+        indexer_names = {}
+
+        for app in apps:
+            app_split = app.split('.')
+            indexer = app_split[0]
+            es_type = app_split[1] if len(app_split) > 1 else None
+            if len(app_split) == 1:
+                indexer_names[indexer] = ['*']
+            elif indexer not in indexer_names:
+                indexer_names[indexer] = [es_type]
+            else:
+                indexer_names[indexer].append(es_type)
+        return indexer_names
+
+    def _get_indexer_names_from_azure(self):
+        block_blob_service = BlockBlobService(
+            account_name=settings.DATA_PIPELINE_STORAGE_ACCOUNT_NAME,
+            account_key=settings.DATA_PIPELINE_STORAGE_ACCOUNT_KEY
+        )
+        rebuild_index_blob = block_blob_service.get_blob_to_text('rebuild-index', 'indexers.json')
+        return json.loads(rebuild_index_blob.content)
 
     def get_indexers(self, **options):
         autodiscover_modules('indexers')
-
         indexers = []
         if len(options['app']):
-            for app in options['app']:
-                app_split = app.split('.')
-                if len(app_split) == 1:
-                    indexers = indexers + list(indexer_klasses_map[app])
-                else:
-                    for indexer in indexer_klasses_map[app_split[0]]:
-                        if indexer.doc_type_klass._doc_type.name == app_split[1]:
-                            indexers.append(indexer)
+            indexer_names = self._get_indexer_names_from_args(options['app'])
+        elif options['from_file']:
+            indexer_names = self._get_indexer_names_from_json(options['from_file'])
+        elif options['from_azure']:
+            indexer_names = self._get_indexer_names_from_azure()
         else:
-            indexers = indexer_klasses
+            return indexer_klasses
+
+        for indexer_name, doc_types in indexer_names.items():
+            list_indexer = indexer_klasses_map[indexer_name]
+            if '*' in doc_types:
+                indexers += list(list_indexer)
+                continue
+            for doc_type in doc_types:
+                indexers += [idx for idx in list_indexer
+                             if idx.doc_type_klass._doc_type.name == doc_type]
         return indexers
 
     def categorize_indexers_by_index_alias(self, indexers):
@@ -41,31 +89,19 @@ class Command(BaseCommand):
     def _get_distinct_doc_types_from_indexers(self, indexers):
         return list(set(x.doc_type_klass._doc_type.name for x in indexers))
 
-    def _migrate(self, index_alias, migrate_doc_types=[]):
-        if not migrate_doc_types:
-            return
-
-        index_alias.write_index.open()
-        query = {}
-        query['source'] = {'index': index_alias.name, 'type': migrate_doc_types}
-        query['dest'] = {
-            'index': index_alias.new_index_name,
-            'version_type': 'external'
-        }
-        es_client.reindex(query)
-
     def handle(self, *args, **options):
         selected_indexers = self.get_indexers(**options)
         alias_indexers_tuple = self.categorize_indexers_by_index_alias(selected_indexers)
 
         for alias, indexers, migrating_indexers in alias_indexers_tuple:
             with alias.indexing():
-
                 list_migrate_doc_types = self._get_distinct_doc_types_from_indexers(migrating_indexers)
                 indexer_klass_instances = [klass() for klass in indexers]
+
                 for indexer_instance in indexer_klass_instances:
                     indexer_instance.create_mapping()
-                self._migrate(alias, list_migrate_doc_types)
+
+                alias.migrate(list_migrate_doc_types)
 
                 for indexer_instance in indexer_klass_instances:
                     indexer_instance.add_new_data()
--- cpdb/es_index/tests/commands/test_rebuild_index.py
+++ cpdb/es_index/tests/commands/test_rebuild_index.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.core.management import call_command
 
-from mock import Mock, patch
+from mock import Mock, patch, mock_open
 
 from es_index import indexer_klasses, indexer_klasses_map
 
@@ -18,6 +18,7 @@ class RebuildIndexCommandTestCase(SimpleTestCase):
         Indexer.index_alias.name = 'test'
         Indexer.index_alias.indexing.return_value.__exit__ = Mock()
         Indexer.index_alias.indexing.return_value.__enter__ = Mock()
+        Indexer.index_alias.migrate = Mock()
         indexer_klasses_map.setdefault('test', set()).clear()
         indexer_klasses_map['test'].add(Indexer)
         indexer_klasses.clear()
@@ -92,19 +93,80 @@ class RebuildIndexCommandTestCase(SimpleTestCase):
 
         Indexer2.doc_type_klass._doc_type.name = 'b'
         Indexer2.index_alias.name = 'test'
+        Indexer2.index_alias.migrate = Mock()
         Indexer2.create_mapping = Mock()
         Indexer2.add_new_data = Mock()
         indexer_klasses_map['test'].add(Indexer2)
         indexer_klasses.add(Indexer2)
 
-        with patch('es_index.es_client.reindex') as mock_reindex:
-            with patch('es_index.management.commands.rebuild_index.autodiscover_modules'):
-                call_command('rebuild_index', 'test.a')
-
-            mock_reindex.assert_called_once()
+        with patch('es_index.management.commands.rebuild_index.autodiscover_modules'):
+            call_command('rebuild_index', 'test.a')
 
         Indexer1.create_mapping.assert_called_once()
         Indexer1.add_new_data.assert_called_once()
-        Indexer2.add_new_data.assert_not_called()
         Indexer1.index_alias.indexing.return_value.__exit__.assert_called_once()
         Indexer1.index_alias.indexing.return_value.__enter__.assert_called_once()
+
+        Indexer2.add_new_data.assert_not_called()
+        Indexer2.index_alias.migrate.assert_not_called()
+
+    def test_call_alias_indexing_with_multiple_doc_type_and_another_alias(self):
+        Indexer1 = self._prepare_data()
+
+        class Indexer2:
+            index_alias = Mock(new_index_name='new_name')
+            doc_type_klass = Mock(_doc_type=Mock())
+        Indexer2.doc_type_klass._doc_type.name = 'b'
+        Indexer2.index_alias.name = 'test'
+        Indexer2.index_alias.migrate = Mock()
+        Indexer2.create_mapping = Mock()
+        Indexer2.add_new_data = Mock()
+        Indexer2.index_alias.indexing.return_value.__exit__ = Mock()
+        Indexer2.index_alias.indexing.return_value.__enter__ = Mock()
+        indexer_klasses_map['test'].add(Indexer2)
+        indexer_klasses.add(Indexer2)
+
+        class Indexer3:
+            index_alias = Mock()
+            doc_type_klass = Mock(_doc_type=Mock())
+        Indexer3.doc_type_klass._doc_type.name = 'c'
+        Indexer3.index_alias.name = 'test2'
+        Indexer3.index_alias.migrate = Mock()
+        Indexer3.index_alias.indexing.return_value.__exit__ = Mock()
+        Indexer3.index_alias.indexing.return_value.__enter__ = Mock()
+        Indexer3.create_mapping = Mock()
+        Indexer3.add_new_data = Mock()
+        indexer_klasses_map['test2'] = set()
+        indexer_klasses_map['test2'].add(Indexer3)
+        indexer_klasses.add(Indexer3)
+
+        with patch('es_index.management.commands.rebuild_index.autodiscover_modules'):
+            call_command('rebuild_index', 'test.a', 'test.b', 'test2')
+
+        Indexer1.create_mapping.assert_called_once()
+        Indexer1.add_new_data.assert_called_once()
+
+        Indexer2.add_new_data.assert_called()
+        Indexer2.index_alias.migrate.assert_called()
+
+        Indexer3.create_mapping.assert_called_once()
+        Indexer3.add_new_data.assert_called_once()
+
+    def test_rebuild_index_from_file(self):
+        Indexer1 = self._prepare_data()
+        with patch('__builtin__.open', mock_open(read_data='{"test": ["*"]}')) as mock_file:
+            call_command('rebuild_index', '--from-file=indexers.json')
+            mock_file.assert_called_with('indexers.json')
+        Indexer1.create_mapping.assert_called_once()
+        Indexer1.index_alias.migrate.assert_called_once()
+        Indexer1.add_new_data.assert_called_once()
+
+    def test_rebuild_index_from_azure(self):
+        Indexer1 = self._prepare_data()
+        with patch('azure.storage.blob.baseblobservice.BaseBlobService.get_blob_to_text',
+                   return_value=Mock(content='{"test": ["*"]}')) as mock:
+            call_command('rebuild_index', '--from-azure')
+            mock.assert_called_once()
+        Indexer1.create_mapping.assert_called_once()
+        Indexer1.index_alias.migrate.assert_called_once()
+        Indexer1.add_new_data.assert_called_once()
