42889d636f92480c2faa6acc42ef63e36ed1f9ec
ea-open-source <open-source@eastagile.com>
2018-04-26 08:35:47 +0700

    [f] 968 - map percentile fields as float type
    [#156115968] https://www.pivotaltracker.com/story/show/156115968

--- cpdb/cr/doc_types.py
+++ cpdb/cr/doc_types.py
@@ -1,4 +1,4 @@
-from elasticsearch_dsl import DocType, Keyword, GeoPoint, Nested, Integer
+from elasticsearch_dsl import DocType, Keyword, GeoPoint, Nested, Integer, Float
 
 from .index_aliases import cr_index_alias
 
@@ -10,5 +10,17 @@ class CRDocType(DocType):
     category_names = Keyword()
     coaccused = Nested(
         properties={
+            'percentile_allegation': Float(),
+            'percentile_allegation_civilian': Float(),
+            'percentile_allegation_internal': Float(),
+            'percentile_trr': Float(),
+            'id': Integer()
+        })
+    involvements = Nested(
+        properties={
+            'percentile_allegation': Float(),
+            'percentile_allegation_civilian': Float(),
+            'percentile_allegation_internal': Float(),
+            'percentile_trr': Float(),
             'id': Integer()
         })
