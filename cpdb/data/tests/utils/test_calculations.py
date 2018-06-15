f817a2e175eff04b34b55e82588e0cc90ecd3916
ea-open-source <open-source@eastagile.com>
2018-06-15 15:08:17 +0700

    [r] 048 - Make percentile calculation related tests consistant
    [#157926048] https://www.pivotaltracker.com/story/show/157926048

--- cpdb/data/tests/utils/test_calculations.py
+++ cpdb/data/tests/utils/test_calculations.py
@@ -64,33 +64,45 @@ class PercentileTestCase(SimpleTestCase):
 
 class CalculateTopPercentileTestCase(TestCase):
     def test_calculate_top_percentile(self):
-        allegation = AllegationFactory()
-        officer = OfficerFactory(appointed_date=date(2002, 1, 1))
-        officer1 = OfficerFactory(appointed_date=date(2000, 1, 1))
-        officer2 = OfficerFactory(appointed_date=date(2001, 1, 1))
-        OfficerAllegationFactory(officer=officer, allegation=allegation, final_finding='SU')
-        OfficerAllegationFactory(officer=officer1, allegation=allegation, final_finding='SU')
-        OfficerAllegationFactory(officer=officer2, allegation=allegation, final_finding='NS')
-        TRRFactory(officer=officer1, trr_datetime=datetime(2017, 1, 3, tzinfo=pytz.utc))
-        TRRFactory(officer=officer2, trr_datetime=datetime(2017, 1, 3, tzinfo=pytz.utc))
+        officer1 = OfficerFactory(appointed_date=date(2001, 1, 1))
+        officer2 = OfficerFactory(appointed_date=date(2002, 1, 1))
+        officer3 = OfficerFactory(appointed_date=date(2003, 1, 1))
+
+        allegation1 = AllegationFactory(incident_date=datetime(2002, 1, 1, tzinfo=pytz.utc))
+        allegation2 = AllegationFactory(incident_date=datetime(2003, 1, 1, tzinfo=pytz.utc))
+        allegation3 = AllegationFactory(incident_date=datetime(2004, 1, 1, tzinfo=pytz.utc))
+
+        OfficerAllegationFactory(
+            officer=officer2, allegation=allegation1, final_finding='SU', start_date=date(2003, 1, 1)
+        )
+        OfficerAllegationFactory(
+            officer=officer3, allegation=allegation2, final_finding='SU', start_date=date(2004, 1, 1)
+        )
+        OfficerAllegationFactory(
+            officer=officer3, allegation=allegation3, final_finding='NS', start_date=date(2005, 1, 1)
+        )
+
+        TRRFactory(officer=officer2, trr_datetime=datetime(2004, 1, 1, tzinfo=pytz.utc))
+        TRRFactory(officer=officer3, trr_datetime=datetime(2005, 1, 1, tzinfo=pytz.utc))
+        TRRFactory(officer=officer3, trr_datetime=datetime(2006, 1, 1, tzinfo=pytz.utc))
 
         expect(calculate_top_percentile()).to.eq({
-            officer.id: {
-                'percentile_allegation_civilian': 66.6667,
-                'percentile_allegation_internal': 0,
-                'percentile_trr': 0,
-                'percentile_allegation': 66.6667,
-            },
             officer1.id: {
                 'percentile_allegation_civilian': 0,
                 'percentile_allegation_internal': 0,
-                'percentile_trr': 33.3333,
+                'percentile_trr': 0,
                 'percentile_allegation': 0,
             },
             officer2.id: {
                 'percentile_allegation_civilian': 33.3333,
                 'percentile_allegation_internal': 0,
-                'percentile_trr': 66.6667,
+                'percentile_trr': 33.3333,
                 'percentile_allegation': 33.3333,
+            },
+            officer3.id: {
+                'percentile_allegation_civilian': 66.6667,
+                'percentile_allegation_internal': 0,
+                'percentile_trr': 66.6667,
+                'percentile_allegation': 66.6667,
             },
         })
