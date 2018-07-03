e2f4427e4634b8744f8e1f5b8c9e2f60e56cef63
ea-open-source <open-source@eastagile.com>
2018-07-03 15:44:18 +0700

    [b] 016 - Fix percentile calculation for retired officer
    [#158758016](https://www.pivotaltracker.com/story/show/158758016)

--- cpdb/data/models.py
+++ cpdb/data/models.py
@@ -381,8 +381,10 @@ class Officer(TaggableModel):
     def _annotate_officer_working_range(query, dataset_min_date, dataset_max_date):
         query = query.annotate(
             end_date=models.Case(
-                models.When(resignation_date__isnull=True, then=models.Value(dataset_max_date)),
-                default='resignation_date',
+                models.When(
+                    Q(resignation_date__isnull=False, resignation_date__lt=dataset_max_date),
+                    then='resignation_date'),
+                default=models.Value(dataset_max_date),
                 output_field=models.DateField()),
             start_date=models.Case(
                 models.When(appointed_date__lt=dataset_min_date, then=models.Value(dataset_min_date)),
@@ -457,6 +459,7 @@ class Officer(TaggableModel):
 
         if year_end:
             dataset_max_date = min(dataset_max_date, date(year_end, 12, 31))
+            dataset_min_date = min(dataset_min_date, date(year_end, 12, 31))
 
         # STEP 1: compute the service time of all officers
         query = Officer.objects.filter(appointed_date__isnull=False)
@@ -510,6 +513,7 @@ class Officer(TaggableModel):
 
         if year_end:
             dataset_max_date = min(dataset_max_date, date(year_end, 12, 31))
+            dataset_min_date = min(dataset_min_date, date(year_end, 12, 31))
 
         # STEP 1: compute the service time of all officers
         query = Officer.objects.filter(appointed_date__isnull=False)
