26920f4db7814040e551b90274c7807231e929df
ea-open-source <open-source@eastagile.com>
2018-07-24 18:12:22 -0500

    [f] 754 - Reverting generate_visual_tokens command to render static image
    [#155769754] https://www.pivotaltracker.com/story/show/155769754

--- cpdb/visual_token/management/commands/generate_visual_tokens.py
+++ cpdb/visual_token/management/commands/generate_visual_tokens.py
@@ -1,51 +1,58 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-import moviepy.editor as mpy
+from tqdm import tqdm
 
-from visual_token.renderer import make_draw_frame
+from visual_token.figures import ChartFigure
 from visual_token.utils import clear_folder
 from officers.doc_types import OfficerInfoDocType
 
 
+PERCENTILE_KEYS = [
+    'percentile_allegation_civilian', 'percentile_allegation_internal', 'percentile_trr'
+]
+CHART_DIMENSIONS = (640, 640)
+
+
 class Command(BaseCommand):
     def has_enough_data(self, data):
-        percentile_keys_set = set(
-            ['percentile_allegation_civilian', 'percentile_allegation_internal', 'percentile_trr']
-        )
-        data_key_set = set(data.keys())
-        return len(data_key_set & percentile_keys_set) >= 2
+        vals = [
+            float(data.get(key, 0))
+            for key in PERCENTILE_KEYS
+        ]
+        vals = [
+            val for val in vals
+            if val > 0
+        ]
+        return len(vals) >= 2
 
     def handle(self, *args, **options):
         clear_folder(settings.VISUAL_TOKEN_SOCIAL_MEDIA_FOLDER)
-        fps = 40
-        frame_per_year = 20
+        figure = ChartFigure(*CHART_DIMENSIONS)
 
-        for officer in OfficerInfoDocType().search().query(
-                'bool',
-                should=[
-                    {'range': {'allegation_count': {'gt': 0}}},
-                    {'range': {'trr_count': {'gt': 0}}}
-                ]).scan():
+        query = OfficerInfoDocType().search().query(
+            'bool',
+            should=[
+                {'range': {'allegation_count': {'gt': 0}}},
+                {'range': {'trr_count': {'gt': 0}}}
+            ])
+        pbar = tqdm(total=query.count())
+        for officer in query.scan():
             officer_dict = officer.to_dict()
             officer_percentiles = officer_dict.get('percentiles', [])
-            if len(officer_percentiles) > 0:
-                chart_data = [
-                    {
-                        'percentile_allegation_civilian': float(data.get('percentile_allegation_civilian', 0)),
-                        'percentile_allegation_internal': float(data.get('percentile_allegation_internal', 0)),
-                        'percentile_trr': float(data.get('percentile_trr', 0))
-                    }
-                    for data in officer_percentiles
-                    if self.has_enough_data(data)
-                ]
+            pbar.update()
 
-                duration = float(frame_per_year) * (len(chart_data) - 1) / fps
-                clip = mpy.VideoClip(
-                    make_draw_frame(chart_data, duration),
-                    duration=duration
-                )
-                clip.write_videofile(
-                    '%s/officer_%s.mp4' % (settings.VISUAL_TOKEN_SOCIAL_MEDIA_FOLDER, officer_dict['id']),
-                    fps=fps
-                )
+            chart_data = [
+                [
+                    float(data.get(key, 0))
+                    for key in PERCENTILE_KEYS
+                ]
+                for data in officer_percentiles
+                if self.has_enough_data(data)
+            ]
+            if len(chart_data) == 0:
+                continue
+
+            surface = figure.draw(chart_data[-1])
+            surface.write_to_png(
+                '%s/officer_%s.png' % (settings.VISUAL_TOKEN_SOCIAL_MEDIA_FOLDER, officer_dict['id']))
---
+++ cpdb/visual_token/tests/management/commands/test_generate_visual_tokens.py
@@ -0,0 +1,38 @@
+from django.test import SimpleTestCase, override_settings
+from django.core.management import call_command
+
+from mock import patch, Mock
+
+from officers.doc_types import OfficerInfoDocType
+from officers.index_aliases import officers_index_alias
+
+
+class CommandTestCase(SimpleTestCase):
+    figure = Mock()
+
+    @patch('visual_token.management.commands.generate_visual_tokens.ChartFigure', return_value=figure)
+    @patch('visual_token.management.commands.generate_visual_tokens.clear_folder')
+    @override_settings(VISUAL_TOKEN_SOCIAL_MEDIA_FOLDER='/media')
+    def test_invocation(self, clear_folder, ChartFigure):
+        OfficerInfoDocType(
+            id='1234',
+            allegation_count=10,
+            trr_count=12,
+            percentiles=[
+                {
+                    'percentile_allegation_civilian': '62.175',
+                    'percentile_allegation_internal': '60.545',
+                    'percentile_trr': '58.606'
+                }
+            ]
+        ).save(index=officers_index_alias.name)
+        officers_index_alias.read_index.refresh()
+
+        surface = Mock()
+        self.figure.draw = Mock(return_value=surface)
+
+        call_command('generate_visual_tokens')
+        clear_folder.assert_called_with('/media')
+        ChartFigure.assert_called_with(640, 640)
+        self.figure.draw.assert_called_with([62.175, 60.545, 58.606])
+        surface.write_to_png.assert_called_with('/media/officer_1234.png')
