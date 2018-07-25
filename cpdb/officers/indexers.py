9935412d48daadbb9ca5e864c3b1d8828dff2f00
ea-open-source <open-source@eastagile.com>
2018-07-25 08:00:54 -0500

    [h] - Remove social graph indexer and add priority to indexers

--- cpdb/officers/indexers.py
+++ cpdb/officers/indexers.py
@@ -1,12 +1,10 @@
-from itertools import combinations
-
 from django.db.models import F, Q
 
 from tqdm import tqdm
 
 from data import officer_percentile
 from data.constants import MIN_VISUAL_TOKEN_YEAR, MAX_VISUAL_TOKEN_YEAR
-from data.models import Officer, OfficerAllegation, OfficerHistory, Allegation, Award
+from data.models import Officer, OfficerAllegation, OfficerHistory, Award
 from data.utils.calculations import calculate_top_percentile
 from es_index import register_indexer
 from es_index.indexers import BaseIndexer
@@ -17,7 +15,6 @@ from officers.serializers.doc_serializers import (
 )
 from trr.models import TRR
 from .doc_types import (
-    OfficerSocialGraphDocType,
     OfficerNewTimelineEventDocType,
     OfficerInfoDocType,
     OfficerCoaccusalsDocType,
@@ -45,52 +42,6 @@ class OfficersIndexer(BaseIndexer):
 
     def extract_datum(self, datum):
         return OfficerInfoSerializer(datum).data
-
-
-@register_indexer(app_name)
-class SocialGraphIndexer(BaseIndexer):
-    doc_type_klass = OfficerSocialGraphDocType
-    index_alias = officers_index_alias
-
-    def get_queryset(self):
-        return Officer.objects.all()
-
-    def _cr_years(self, officer):
-        dates = officer.officerallegation_set.values_list('allegation__incident_date', flat=True)
-        return sorted([_date.year if _date is not None else None for _date in dates])
-
-    def _node(self, officer):
-        return {
-            "id": officer.id,
-            "name": officer.full_name,
-            "cr_years": self._cr_years(officer)
-        }
-
-    def _links(self, officers):
-        links = []
-        for o1, o2 in combinations(officers, 2):
-            qs = Allegation.objects.filter(officerallegation__officer=o1) \
-                .filter(officerallegation__officer=o2).distinct()
-            if qs.exists():
-                link = {
-                    'source': o1.id,
-                    'target': o2.id,
-                    'cr_years': sorted([
-                        _date.year if _date is not None else None
-                        for _date in qs.values_list('incident_date', flat=True)
-                    ])
-                }
-                links.append(link)
-        return links
-
-    def extract_datum(self, officer):
-        coaccuseds = Officer.objects.filter(
-            officerallegation__allegation__officerallegation__officer=officer).distinct().order_by('id')
-
-        return {'officer_id': officer.pk, 'graph': {
-            'links': self._links(coaccuseds),
-            'nodes': [self._node(coaccused) for coaccused in coaccuseds]
-        }}
 
 
 @register_indexer(app_name)
