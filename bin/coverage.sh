527337163791b60f87e198bbaff2ee19303ce372
ea-open-source <open-source@eastagile.com>
2018-08-01 09:59:16 +0700

    [c] Improve test coverage on develop branch

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-coverage run ./cpdb/manage.py test && coverage report -m cpdb/**/*.py && coverage html
+coverage run ./cpdb/manage.py test && coverage combine && coverage report --omit="/home/ubuntu/virtualenvs/*"
