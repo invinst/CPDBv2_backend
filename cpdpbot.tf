3adb2093c36924b2e226efa459f7753ce636073a
ea-open-source <open-source@eastagile.com>
2018-08-08 14:56:41 +0700

    [f] 156 - Configure CircleCI to test and deploy cpdpbot
    [#159519156] https://www.pivotaltracker.com/story/show/159519156

--- cpdpbot.tf
+++ cpdpbot.tf
@@ -2,6 +2,7 @@ variable "staging_twitter_consumer_key" {}
 variable "staging_twitter_consumer_secret" {}
 variable "staging_twitter_app_token_key" {}
 variable "staging_twitter_app_token_secret" {}
+variable "cpdpbot_version" {}
 
 resource "kubernetes_replication_controller" "cpdpbot_staging" {
   metadata {
@@ -18,7 +19,7 @@ resource "kubernetes_replication_controller" "cpdpbot_staging" {
     }
     template {
       container {
-        image = "cpdbdev/cpdpbot:latest"
+        image = "cpdbdev/cpdpbot:${var.cpdpbot_version}"
         name  = "cpdpbot"
         env   = [
           {
@@ -54,47 +55,3 @@ resource "kubernetes_replication_controller" "cpdpbot_staging" {
     }
   }
 }
-
-# resource "kubernetes_pod" "cpdpbot_staging" {
-#   metadata {
-#     name      = "cpdpbot"
-#     namespace = "${kubernetes_namespace.staging.metadata.0.name}"
-#   }
-
-#   spec {
-#     container {
-#       image = "cpdbdev/cpdpbot:0.1.0"
-#       name  = "cpdpbot"
-#       env   = [
-#         {
-#           name  = "AZURE_QUEUE_NAME"
-#           value = "cpdpbot"
-#         },
-#         {
-#           name  = "TWITTER_CONSUMER_KEY"
-#           value = "${var.staging_twitter_consumer_key}"
-#         },
-#         {
-#           name  = "TWITTER_CONSUMER_SECRET"
-#           value = "${var.staging_twitter_consumer_secret}"
-#         },
-#         {
-#           name  = "TWITTER_APP_TOKEN_KEY"
-#           value = "${var.staging_twitter_app_token_key}"
-#         },
-#         {
-#           name  = "TWITTER_APP_TOKEN_SECRET"
-#           value = "${var.staging_twitter_app_token_secret}"
-#         },
-#         {
-#           name  = "AZURE_STORAGE_ACCOUNT_NAME"
-#           value = "${azurerm_storage_account.staging.name}"
-#         },
-#         {
-#           name  = "AZURE_STORAGE_ACCOUNT_KEY"
-#           value = "${azurerm_storage_account.staging.primary_access_key}"
-#         },
-#       ]
-#     }
-#   }
-# }
