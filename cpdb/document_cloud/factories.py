ae6051ffa56ea8bf1a94390f8f7ee30fc573ba09
ea-open-source <open-source@eastagile.com>
2018-08-14 15:29:58 +0700

    [c] - Fix error: object of type 'generator' has no len()

--- cpdb/document_cloud/factories.py
+++ cpdb/document_cloud/factories.py
@@ -10,5 +10,5 @@ fake = Faker()
 class DocumentCloudSearchQueryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = DocumentCloudSearchQuery
-    type = factory.Sequence(lambda n: fake.random_element(x[0] for x in DOCUMENT_TYPES))
+    type = factory.Sequence(lambda n: fake.random_element([x[0] for x in DOCUMENT_TYPES]))
     query = factory.Sequence(lambda n: str(n))
