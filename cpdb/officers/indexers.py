fde4d5c7fe0fc85fc53a80faa6e8c442ac3b3f52
ea-open-source <open-source@eastagile.com>
2018-09-10 11:34:31 +0700

    [c] 983 - Light refactor to OfficersIndexer
    [#160088983] https://www.pivotaltracker.com/story/show/160088983

--- cpdb/officers/indexers.py
+++ cpdb/officers/indexers.py
@@ -2,8 +2,6 @@ import itertools
 
 from django.db.models import F, Q
 
-from tqdm import tqdm
-
 from data import officer_percentile
 from data.constants import (
     MIN_VISUAL_TOKEN_YEAR, MAX_VISUAL_TOKEN_YEAR,
@@ -11,6 +9,7 @@ from data.constants import (
 )
 from data.models import Officer, OfficerAllegation, OfficerHistory, Award, Salary
 from es_index import register_indexer
+from es_index.utils import timing_validate
 from es_index.indexers import BaseIndexer, PartialIndexer
 from es_index.serializers import get_gender, get_age_range
 from trr.models import TRR
@@ -40,8 +39,6 @@ class OfficersIndexer(BaseIndexer):
     doc_type_klass = OfficerInfoDocType
     index_alias = officers_index_alias
     serializer = OfficerSerializer()
-    query = OfficerQuery()
-    allegation_query = AllegationQuery()
     percentile_groups = [
         PERCENTILE_ALLEGATION_GROUP,
         PERCENTILE_ALLEGATION_INTERNAL_CIVILIAN_GROUP,
@@ -50,9 +47,14 @@ class OfficersIndexer(BaseIndexer):
 
     def __init__(self):
         super(OfficersIndexer, self).__init__()
+        self.populate_top_percentile_dict()
+        self.populate_allegation_dict()
+        self.populate_award_dict()
 
+    @timing_validate('OfficersIndexer: Preparing percentile data...')
+    def populate_top_percentile_dict(self):
         self.yearly_top_percentile = dict()
-        for yr in tqdm(range(MIN_VISUAL_TOKEN_YEAR, MAX_VISUAL_TOKEN_YEAR + 1), desc='Prepare percentile data'):
+        for yr in range(MIN_VISUAL_TOKEN_YEAR, MAX_VISUAL_TOKEN_YEAR + 1):
             for officer in officer_percentile.top_percentile(yr, percentile_groups=self.percentile_groups):
                 if officer.resignation_date and yr > officer.resignation_date.year:
                     continue
@@ -70,11 +72,9 @@ class OfficersIndexer(BaseIndexer):
                         officer_dict[attr] = str(round(getattr(officer, attr), 4))
                 officer_list.append(officer_dict)
 
-    def get_queryset(self):
-        return Officer.objects.all()
-
-    def get_query(self):
-        allegations = self.allegation_query.execute()
+    @timing_validate('OfficersIndexer: Populating allegation dict...')
+    def populate_allegation_dict(self):
+        allegations = AllegationQuery().execute()
         self.allegation_dict = dict()
         self.coaccusals = dict()
         transform_gender = get_gender('gender', 'Unknown')
@@ -95,12 +95,18 @@ class OfficersIndexer(BaseIndexer):
                 dict_b = self.coaccusals.setdefault(officer_id_b, dict())
                 dict_b[officer_id_a] = dict_b.get(officer_id_a, 0) + 1
 
+    @timing_validate('OfficersIndexer: Populating award dict...')
+    def populate_award_dict(self):
         self.award_dict = dict()
         awards = AwardQuery().execute()
         for award in awards:
             self.award_dict.setdefault(award['officer_id'], []).append(award)
 
-        return self.query.execute()
+    def get_queryset(self):
+        return Officer.objects.all()
+
+    def get_query(self):
+        return OfficerQuery().execute()
 
     def extract_datum(self, datum):
         datum['allegations'] = self.allegation_dict.get(datum['id'], [])
