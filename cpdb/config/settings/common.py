849a0be745fa774345b6251fe6cd5a37b4db7454
ea-open-source <open-source@eastagile.com>
2018-09-25 08:33:45 +0700

    [c] 929 - Set env var during docker run
    [#160607929] https://www.pivotaltracker.com/story/show/160607929

--- cpdb/config/settings/common.py
+++ cpdb/config/settings/common.py
@@ -13,7 +13,7 @@ environ.Env.read_env("{root}/.env".format(root=ROOT_DIR))  # reading .env file
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('DJANGO_SECRET_KEY')
+SECRET_KEY = env.str('DJANGO_SECRET_KEY', default='django')
 
 ALLOWED_HOSTS = ['*']
 
