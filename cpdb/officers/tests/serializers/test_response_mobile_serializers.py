f422ba1bdbd46a06e6779287d064c53f7288d0c6
ea-open-source <open-source@eastagile.com>
2018-10-19 17:08:04 +0700

    [f] 412 - Mobile - Get data from Postgres instead of elasticsearch for officer API
    [#160951412](https://www.pivotaltracker.com/story/show/160951412)

--- cpdb/officers/tests/serializers/test_response_mobile_serializers.py
+++ cpdb/officers/tests/serializers/test_response_mobile_serializers.py
@@ -1,209 +1,368 @@
-from datetime import datetime
+from datetime import date, datetime
 
-from django.test import SimpleTestCase
-from mock import Mock
+from django.test import TestCase
+from django.contrib.gis.geos import Point
+
+import pytz
 from robber import expect
 
 from officers.serializers.response_mobile_serializers import (
-    OfficerMobileSerializer,
-    OfficerYearlyPercentileMobileSerializer,
-    MobileTimelineSerializer
+    OfficerInfoMobileSerializer,
+    PoliceUnitMobileSerializer,
+    BaseTimelineMobileSerializer,
+    RankChangeNewTimelineMobileSerializer,
+    JoinedNewTimelineMobileSerializer,
+    UnitChangeNewTimelineMobileSerializer,
+    CRNewTimelineMobileSerializer,
+    AwardNewTimelineMobileSerializer,
+    TRRNewTimelineMobileSerializer,
 )
+from data.factories import (
+    OfficerFactory, PoliceUnitFactory, OfficerBadgeNumberFactory,
+    OfficerHistoryFactory, OfficerYearlyPercentileFactory, SalaryFactory,
+    AllegationFactory, OfficerAllegationFactory, AllegationCategoryFactory,
+    AttachmentFileFactory, VictimFactory, AwardFactory
+)
+from trr.factories import TRRFactory
 
 
-class MobileTimeLineMobileSerializerTestCase(SimpleTestCase):
+class PoliceUnitMobileSerializerTestCase(TestCase):
     def test_serialization(self):
-        obj = Mock()
-        obj.to_dict = Mock(return_value={
-            'a': 'b',
-            'c': 'd',
-            'date_sort': datetime.now(),
-            'priority_sort': 40,
-            'officer_id': 123,
-            'attachments': [{
-                'file_type': 'document',
-                'url': 'https://www.documentcloud.org/documents/3518955-CRID-303350-CR.pdf'
-            }]
-        })
-        expect(MobileTimelineSerializer(obj).data).to.eq({
-            'a': 'b',
-            'c': 'd',
-            'attachments': [{
-                'file_type': 'document',
-                'url': 'https://www.documentcloud.org/documents/3518955-CRID-303350-CR.pdf'
-            }]
+        unit_with_name = PoliceUnitFactory(
+            id=123,
+            unit_name='004',
+            description='District 004',
+        )
+
+        expect(PoliceUnitMobileSerializer(unit_with_name).data).to.eq({
+            'unit_id': 123,
+            'unit_name': '004',
+            'description': 'District 004',
         })
 
-    def test_serialization_multiple(self):
-        obj1 = Mock()
-        obj1.to_dict = Mock(return_value={
-            'a': 'b',
-            'date_sort': datetime.now(),
-            'priority_sort': 40,
-            'officer_id': 123,
-            'attachments': [{
-                'file_type': 'document',
-            }]
-        })
-        obj2 = Mock()
-        obj2.to_dict = Mock(return_value={
-            'a': 'b',
-            'c': 'd',
-            'date_sort': datetime.now(),
-            'priority_sort': 40,
-            'officer_id': 123,
-            'attachments': [{
-                'file_type': 'document',
-                'url': 'https://www.documentcloud.org/documents/3518955-CRID-303350-CR.pdf'
-            }]
-        })
-        expect(MobileTimelineSerializer([obj1, obj2], many=True).data).to.eq([{
-            'a': 'b',
-            'attachments': [{
-                'file_type': 'document',
-            }]
-        }, {
-            'a': 'b',
-            'c': 'd',
-            'attachments': [{
-                'file_type': 'document',
-                'url': 'https://www.documentcloud.org/documents/3518955-CRID-303350-CR.pdf'
-            }]
-        }])
 
-
-class OfficerMobileSerializerTestCase(SimpleTestCase):
+class OfficerInfoMobileSerializerTestCase(TestCase):
     def test_serialization(self):
-        obj = Mock(**{
-            'id': 789,
-            'last_unit': Mock(id=1, unit_name='', description=''),
-            'date_of_appt': '01-01-2010',
-            'date_of_resignation': '01-01-2015',
-            'active': True,
-            'rank': '',
-            'full_name': 'Full Name',
-            'race': 'Asian',
-            'badge': '789',
-            'historic_badges': ['123', '456'],
-            'unit': Mock(**{
-                'id': 1,
-                'unit_name': '1',
-                'description': 'Unit 1'
-            }),
-            'gender': 'Male',
-            'birth_year': 1950,
-            'allegation_count': 2,
-            'complaint_percentile': 32.5,
-            'honorable_mention_count': 1,
-            'sustained_count': 1,
-            'discipline_count': 1,
-            'civilian_compliment_count': 6,
-            'honorable_mention_percentile': 0.99,
-            'trr_count': 1,
-            'major_award_count': 1,
-            'current_salary': 90000,
-            'percentiles': [
-                Mock(**{
-                    'percentile_allegation': 99.345,
-                    'percentile_trr': 0.000,
-                    'year': 2001,
-                    'id': 1,
-                    'percentile_allegation_civilian': 98.434,
-                    'percentile_allegation_internal': 99.784,
-                }),
-                Mock(**{
-                    'percentile_allegation': 99.345,
-                    'percentile_trr': 0.000,
-                    'year': 2002,
-                    'id': 1,
-                    'percentile_allegation_civilian': 98.434,
-                    'percentile_allegation_internal': 99.784,
-                }),
-            ],
-        })
-        expect(OfficerMobileSerializer(obj).data).to.eq({
-            'officer_id': 789,
-            'full_name': 'Full Name',
-            'percentiles': [
-                {
-                    'percentile_allegation': '99.3450',
-                    'percentile_trr': '0.0000',
-                    'year': 2001,
-                    'id': 1,
-                    'percentile_allegation_civilian': '98.4340',
-                    'percentile_allegation_internal': '99.7840',
-                },
-                {
-                    'percentile_allegation': '99.3450',
-                    'percentile_trr': '0.0000',
-                    'year': 2002,
-                    'id': 1,
-                    'percentile_allegation_civilian': '98.4340',
-                    'percentile_allegation_internal': '99.7840',
-                },
-            ],
+        unit = PoliceUnitFactory(
+            id=4,
+            unit_name='004',
+            description='District 004',
+        )
+        old_unit = PoliceUnitFactory(
+            id=5,
+            unit_name='005',
+            description='District 005',
+        )
+
+        officer = OfficerFactory(
+            id=123,
+            first_name='Michael',
+            last_name='Flynn',
+            last_unit=unit,
+            appointed_date=date(2000, 01, 02),
+            resignation_date=date(2010, 02, 03),
+            active='Yes',
+            rank='Sergeant',
+            race='Black',
+            gender='F',
+            current_badge='456',
+            birth_year=1950,
+            allegation_count=20,
+            complaint_percentile='99.9900',
+            honorable_mention_count=3,
+            sustained_count=4,
+            discipline_count=6,
+            civilian_compliment_count=2,
+            trr_count=7,
+            major_award_count=8,
+            honorable_mention_percentile='88.8800',
+        )
+
+        OfficerBadgeNumberFactory(
+            officer=officer,
+            current=False,
+            star='789'
+        )
+        OfficerBadgeNumberFactory(
+            officer=officer,
+            current=True,
+            star='456'
+        )
+
+        OfficerHistoryFactory(officer=officer, unit=old_unit, effective_date=date(2002, 01, 02))
+        OfficerHistoryFactory(officer=officer, unit=unit, effective_date=date(2004, 01, 02))
+
+        OfficerYearlyPercentileFactory(
+            officer=officer,
+            year=2002,
+            percentile_trr='99.88',
+            percentile_allegation=None,
+            percentile_allegation_civilian='77.66',
+            percentile_allegation_internal='66.55'
+        )
+        OfficerYearlyPercentileFactory(
+            officer=officer,
+            year=2003,
+            percentile_trr='99.99',
+            percentile_allegation='88.88',
+            percentile_allegation_civilian='77.77',
+            percentile_allegation_internal='66.66'
+        )
+
+        expect(OfficerInfoMobileSerializer(officer).data).to.eq({
+            'officer_id': 123,
             'unit': {
-                'unit_id': 1,
-                'unit_name': '1',
-                'description': 'Unit 1'
+                'unit_id': 4,
+                'unit_name': '004',
+                'description': 'District 004',
             },
-            'date_of_appt': '01-01-2010',
-            'date_of_resignation': '01-01-2015',
-            'active': True,
-            'rank': '',
-            'race': 'Asian',
-            'badge': '789',
+            'date_of_appt': '2000-01-02',
+            'date_of_resignation': '2010-02-03',
+            'active': 'Active',
+            'rank': 'Sergeant',
+            'full_name': 'Michael Flynn',
+            'race': 'Black',
+            'badge': '456',
+            'historic_badges': ['789'],
+            'gender': 'Female',
             'birth_year': 1950,
-            'historic_badges': ['123', '456'],
-            'gender': 'Male',
-            'allegation_count': 2,
-            'complaint_percentile': 32.5,
-            'honorable_mention_count': 1,
-            'sustained_count': 1,
-            'discipline_count': 1,
-            'civilian_compliment_count': 6,
-            'trr_count': 1,
-            'major_award_count': 1,
-            'honorable_mention_percentile': 0.99,
+            'allegation_count': 20,
+            'complaint_percentile': 99.99,
+            'honorable_mention_count': 3,
+            'sustained_count': 4,
+            'discipline_count': 6,
+            'civilian_compliment_count': 2,
+            'trr_count': 7,
+            'major_award_count': 8,
+            'honorable_mention_percentile': 88.88,
+            'percentiles': [
+                {
+                    'id': 123,
+                    'year': 2002,
+                    'percentile_trr': '99.8800',
+                    'percentile_allegation_civilian': '77.6600',
+                    'percentile_allegation_internal': '66.5500',
+                },
+                {
+                    'id': 123,
+                    'year': 2003,
+                    'percentile_trr': '99.9900',
+                    'percentile_allegation': '88.8800',
+                    'percentile_allegation_civilian': '77.7700',
+                    'percentile_allegation_internal': '66.6600',
+                },
+            ]
         })
 
-    def test_serialization_missing_data(self):
-        data_dict = {
-            'id': 789,
-            'active': True,
-            'rank': '',
-            'full_name': 'Full Name',
-            'race': 'Asian',
-            'gender': 'Male',
-        }
-        obj = Mock(spec=data_dict.keys(), **data_dict)
-        expected_response = {
-            'officer_id': 789,
-            'full_name': 'Full Name',
-            'active': True,
-            'rank': '',
-            'race': 'Asian',
-            'gender': 'Male',
-        }
-        expect(OfficerMobileSerializer(obj).data).to.eq(expected_response)
+
+class BaseTimelineMobileSerializerMobileSerializerTestCase(TestCase):
+    def test_raise_NotImplementedError(self):
+        expect(lambda: BaseTimelineMobileSerializer().get_kind(None)).to.throw(NotImplementedError)
+        expect(lambda: BaseTimelineMobileSerializer().get_priority_sort(None)).to.throw(NotImplementedError)
 
 
-class OfficerYearlyPercentileMobileSerializerTestCase(SimpleTestCase):
+class RankChangeNewTimelineMobileSerializerTestCase(TestCase):
+    def test_serialization(self):
+        officer = OfficerFactory(id=123)
+        salary = SalaryFactory(
+            officer=officer,
+            spp_date=date(2002, 02, 03),
+            rank='Police Officer',
+        )
+        setattr(salary, 'unit_name', 'Unit 001')
+        setattr(salary, 'unit_description', 'District 001')
 
-    def test_serialization_missing_percentile(self):
-        data = {
-            'id': 1,
-            'year': 2016,
-            'percentile_allegation': '66.6670',
-            'percentile_allegation_internal': '50.0000',
-            'percentile_allegation_civilian': '0.0000',
-        }
-        obj = Mock(spec=data.keys(), **data)
-        expected_response = {
-            'id': 1,
-            'year': 2016,
-            'percentile_allegation': '66.6670',
-            'percentile_allegation_internal': '50.0000',
-            'percentile_allegation_civilian': '0.0000',
-        }
-        expect(OfficerYearlyPercentileMobileSerializer(obj).data).to.eq(expected_response)
+        expect(RankChangeNewTimelineMobileSerializer(salary).data).to.eq({
+            'unit_name': 'Unit 001',
+            'unit_description': 'District 001',
+            'rank': 'Police Officer',
+            'priority_sort': 25,
+            'kind': 'RANK_CHANGE',
+            'date_sort': date(2002, 02, 03),
+            'date': '2002-02-03'
+        })
+
+
+class JoinedNewTimelineMobileSerializerTestCase(TestCase):
+    def test_serialization(self):
+        officer = OfficerFactory(id=123, appointed_date=date(2002, 02, 03))
+        setattr(officer, 'unit_name', 'Unit 001')
+        setattr(officer, 'unit_description', 'District 001')
+        setattr(officer, 'rank_name', 'Police Officer')
+
+        expect(JoinedNewTimelineMobileSerializer(officer).data).to.eq({
+            'unit_name': 'Unit 001',
+            'unit_description': 'District 001',
+            'rank': 'Police Officer',
+            'priority_sort': 10,
+            'kind': 'JOINED',
+            'date_sort': date(2002, 02, 03),
+            'date': '2002-02-03'
+        })
+
+
+class UnitChangeNewTimelineMobileSerializerTestCase(TestCase):
+    def test_serialization(self):
+        officer = OfficerFactory(id=123)
+        unit = PoliceUnitFactory(
+            id=4,
+            unit_name='004',
+            description='District 004',
+        )
+        officer_history = OfficerHistoryFactory(
+            officer=officer,
+            unit=unit,
+            effective_date=date(2002, 02, 03)
+        )
+        setattr(officer_history, 'rank_name', 'Police Officer')
+
+        expect(UnitChangeNewTimelineMobileSerializer(officer_history).data).to.eq({
+            'unit_name': '004',
+            'unit_description': 'District 004',
+            'rank': 'Police Officer',
+            'priority_sort': 20,
+            'kind': 'UNIT_CHANGE',
+            'date_sort': date(2002, 02, 03),
+            'date': '2002-02-03'
+        })
+
+
+class CRNewTimelineMobileSerializerTestCase(TestCase):
+    def test_serialization(self):
+        officer = OfficerFactory(id=123)
+        allegation = AllegationFactory(crid='CR123', coaccused_count=3, point=Point([0.01, 0.02]))
+        allegation_category = AllegationCategoryFactory(
+            category='some category',
+            allegation_name='some sub category'
+        )
+        officer_allegation = OfficerAllegationFactory(
+            officer=officer,
+            allegation=allegation,
+            start_date=date(2002, 02, 03),
+            allegation_category=allegation_category,
+            final_finding='SU',
+            final_outcome='9 Day Suspension'
+        )
+
+        AttachmentFileFactory(
+            allegation=allegation,
+            title='title',
+            url='url',
+            preview_image_url='preview_image_url',
+            file_type='document'
+        )
+        VictimFactory(allegation=allegation, gender='M', race='Black', age=30)
+
+        setattr(officer_allegation, 'unit_name', 'Unit 001')
+        setattr(officer_allegation, 'unit_description', 'District 001')
+        setattr(officer_allegation, 'rank_name', 'Police Officer')
+
+        expect(CRNewTimelineMobileSerializer(officer_allegation).data).to.eq({
+            'unit_name': 'Unit 001',
+            'unit_description': 'District 001',
+            'rank': 'Police Officer',
+            'priority_sort': 30,
+            'kind': 'CR',
+            'date_sort': date(2002, 02, 03),
+            'date': '2002-02-03',
+            'crid': 'CR123',
+            'category': 'some category',
+            'subcategory': 'some sub category',
+            'finding': 'Sustained',
+            'outcome': '9 Day Suspension',
+            'coaccused': 3,
+            'attachments': [
+                {
+                    'title': 'title',
+                    'url': 'url',
+                    'preview_image_url': 'preview_image_url',
+                    'file_type': 'document',
+                }
+            ],
+            'point': {
+                'lon': 0.01,
+                'lat': 0.02
+            },
+            'victims': [
+                {
+                    'gender': 'Male',
+                    'race': 'Black',
+                    'age': 30
+                }
+            ]
+
+        })
+
+    def test_get_point(self):
+        allegation = AllegationFactory(crid='CR123', coaccused_count=3, point=None)
+        officer_allegation = OfficerAllegationFactory(allegation=allegation)
+
+        setattr(officer_allegation, 'unit_name', 'Unit 001')
+        setattr(officer_allegation, 'unit_description', 'District 001')
+        setattr(officer_allegation, 'rank_name', 'Police Officer')
+
+        expect(CRNewTimelineMobileSerializer(officer_allegation).data).to.exclude('point')
+
+
+class AwardNewTimelineMobileSerializerTestCase(TestCase):
+    def test_serialization(self):
+        officer = OfficerFactory(id=123)
+        award = AwardFactory(officer=officer, start_date=date(2002, 02, 03), award_type='Life Saving Award')
+
+        setattr(award, 'unit_name', 'Unit 001')
+        setattr(award, 'unit_description', 'District 001')
+        setattr(award, 'rank_name', 'Police Officer')
+
+        expect(AwardNewTimelineMobileSerializer(award).data).to.eq({
+            'unit_name': 'Unit 001',
+            'unit_description': 'District 001',
+            'rank': 'Police Officer',
+            'priority_sort': 40,
+            'kind': 'AWARD',
+            'date_sort': date(2002, 02, 03),
+            'date': '2002-02-03',
+            'award_type': 'Life Saving Award'
+        })
+
+
+class TRRNewTimelineMobileSerializerTestCase(TestCase):
+    def test_serialization(self):
+        officer = OfficerFactory(id=123)
+        trr = TRRFactory(
+            id=456,
+            officer=officer,
+            trr_datetime=datetime(2002, 02, 03, tzinfo=pytz.utc),
+            point=Point([0.01, 0.02]),
+            taser=True,
+            firearm_used=False
+        )
+
+        setattr(trr, 'unit_name', 'Unit 001')
+        setattr(trr, 'unit_description', 'District 001')
+        setattr(trr, 'rank_name', 'Police Officer')
+
+        expect(TRRNewTimelineMobileSerializer(trr).data).to.eq({
+            'unit_name': 'Unit 001',
+            'unit_description': 'District 001',
+            'rank': 'Police Officer',
+            'priority_sort': 50,
+            'kind': 'FORCE',
+            'trr_id': 456,
+            'date_sort': date(2002, 02, 03),
+            'date': '2002-02-03',
+            'taser': True,
+            'firearm_used': False,
+            'point': {
+                'lon': 0.01,
+                'lat': 0.02
+            }
+        })
+
+    def test_get_point(self):
+        trr = TRRFactory(id=456, point=None)
+
+        setattr(trr, 'unit_name', 'Unit 001')
+        setattr(trr, 'unit_description', 'District 001')
+        setattr(trr, 'rank_name', 'Police Officer')
+
+        expect(TRRNewTimelineMobileSerializer(trr).data).to.exclude('point')
