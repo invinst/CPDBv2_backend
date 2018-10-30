4927f45e7fccf65d6425a11922004e470ee6e3c3
ea-open-source <open-source@eastagile.com>
2018-10-30 09:09:08 +0700

    [f] 466 - Change vm type for kubernetes cluster
    [#160784466] https://www.pivotaltracker.com/story/show/160784466

--- kubernetes.tf
+++ kubernetes.tf
@@ -3,14 +3,8 @@ variable "kubernetes_admin_ssh_pub_key" {}
 variable "kubernetes_client_id" {}
 variable "kubernetes_client_secret" {}
 
-resource azurerm_network_security_group "cpdp_akc_nsg" {
-  name                = "cpdp-akc-nsg"
-  location            = "${azurerm_resource_group.terraformed.location}"
-  resource_group_name = "${azurerm_resource_group.terraformed.name}"
-}
-
-resource "azurerm_kubernetes_cluster" "cpdp_akc" {
-  name                = "cpdp-akc"
+resource "azurerm_kubernetes_cluster" "cpdp_aks_cluster" {
+  name                = "cpdp-aks-cluster"
   location            = "${azurerm_resource_group.terraformed.location}"
   resource_group_name = "${azurerm_resource_group.terraformed.name}"
   dns_prefix          = "cpdp"
@@ -25,8 +19,8 @@ resource "azurerm_kubernetes_cluster" "cpdp_akc" {
 
   agent_pool_profile {
     name            = "default"
-    count           = 3
-    vm_size         = "Standard_D2_v3"
+    count           = 4
+    vm_size         = "Standard_D2s_v3"
     os_type         = "Linux"
     os_disk_size_gb = 30
   }
@@ -38,5 +32,5 @@ resource "azurerm_kubernetes_cluster" "cpdp_akc" {
 }
 
 output "kube_config" {
-    value = "${azurerm_kubernetes_cluster.cpdp_akc.kube_config_raw}"
+    value = "${azurerm_kubernetes_cluster.cpdp_aks_cluster.kube_config_raw}"
 }
