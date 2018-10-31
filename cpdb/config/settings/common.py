886dccfe269d620705880dff76647f1787f3f30c
ea-open-source <open-source@eastagile.com>
2018-10-31 17:12:37 +0700

    [f] 466 - Add profiling capability to gunicorn config
    [#160784466] https://www.pivotaltracker.com/story/show/160784466

--- cpdb/config/settings/common.py
+++ cpdb/config/settings/common.py
@@ -69,7 +69,6 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # ------------------------------------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.cache.UpdateCacheMiddleware',
-    'shared.middleware.ProfileMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
