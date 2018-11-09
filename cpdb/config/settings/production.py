a8c2035826d6c2e892d0a756e65bdbfda87e412b
ea-open-source <open-source@eastagile.com>
2018-11-09 10:44:12 +0700

    [f] 466 - Remove papertrail log handler
    [#160784466] https://www.pivotaltracker.com/story/show/160784466

--- cpdb/config/settings/production.py
+++ cpdb/config/settings/production.py
@@ -1,5 +1,4 @@
 from .common import *  # NOQA
-from .common import LOGGING
 
 
 import environ
@@ -41,29 +40,5 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
-    }
-}
-
-LOGGING['handlers']['Papertrail'] = {
-    'level': 'INFO',
-    'class': 'logging.handlers.SysLogHandler',
-    'formatter': 'standard',
-    'address': (env.str('PAPERTRAIL_ENDPOINT', ''), env.int('PAPERTRAIL_PORT', 40000))
-}
-LOGGING['loggers'] = {
-    'django': {
-        'handlers': ['Papertrail'],
-        'level': 'ERROR',
-        'propagate': True,
-    },
-    'django.command': {
-        'handlers': ['console'],
-        'level': 'INFO',
-        'propagate': True,
-    },
-    'twitterbot': {
-        'handlers': ['Papertrail'],
-        'level': 'INFO',
-        'propagate': True,
     }
 }
