4e06bd35b639d4d05d9bcd7ba5f190af04b68118
ea-open-source <open-source@eastagile.com>
2019-01-05 09:48:05 +0700

    [r] 164 - Add get_officers_most_complaints_from_query
    [#162804164] https://www.pivotaltracker.com/story/show/162804164

--- cpdb/data/models/area.py
+++ cpdb/data/models/area.py
@@ -5,6 +5,7 @@ from django.db.models import F, Value, Count
 from django.db.models.functions import Concat, Cast
 
 from data.constants import AREA_CHOICES
+from data.utils.getters import get_officers_most_complaints_from_query
 from .common import TaggableModel, TimeStampsModel
 
 
@@ -61,25 +62,7 @@ class Area(TimeStampsModel, TaggableModel):
     def get_officers_most_complaints(self):
         OfficerAllegation = apps.get_app_config('data').get_model('OfficerAllegation')
         query = OfficerAllegation.objects.filter(allegation__areas__in=[self])
-        query = query.values('officer').annotate(
-            id=F('officer__id'),
-            percentile_allegation=F('officer__complaint_percentile'),
-            percentile_allegation_civilian=F('officer__civilian_allegation_percentile'),
-            percentile_allegation_internal=F('officer__internal_allegation_percentile'),
-            percentile_trr=F('officer__trr_percentile'),
-            name=Concat('officer__first_name', Value(' '), 'officer__last_name'),
-            count=Count('allegation', distinct=True)
-        )
-        query = query.order_by('-count')[:3]
-        return query.values(
-            'id',
-            'name',
-            'count',
-            'percentile_allegation',
-            'percentile_allegation_civilian',
-            'percentile_allegation_internal',
-            'percentile_trr'
-        )
+        return get_officers_most_complaints_from_query(query)
 
     @property
     def allegation_count(self):
