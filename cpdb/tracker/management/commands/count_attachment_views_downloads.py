61ff5a980cf463427025b02b6d557c6e8479ace4
ea-open-source <open-source@eastagile.com>
2019-02-15 08:59:58 +0700

    [f] 572 - Added cronjob to update attachment views & download
    [#163852572] https://www.pivotaltracker.com/story/show/163852572

--- cpdb/tracker/management/commands/count_attachment_views_downloads.py
+++ cpdb/tracker/management/commands/count_attachment_views_downloads.py
@@ -1,7 +1,8 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from django.db import transaction
+
+from tqdm import tqdm
 
 from data.models import AttachmentFile
 from analytics.models import Event
@@ -10,17 +11,12 @@ logger = logging.getLogger('attachment.count_views_and_downloads')
 
 
 class Command(BaseCommand):
-    help = "Count attachments' views and downloads"
+    help = "Count attachments views and downloads"
 
     def handle(self, *args, **kwargs):
-        logger.info('Attachments views and downloads counting process is about to start...')
-
-        attachments = AttachmentFile.objects.select_for_update().all()
-        with transaction.atomic():
-            for attachment in attachments:
-                views_count = Event.objects.filter(name='attachment-view', data__id=attachment.id).count()
-                downloads_count = Event.objects.filter(name='attachment-download', data__id=attachment.id).count()
-
-                attachment.views_count = views_count
-                attachment.downloads_count = downloads_count
-                attachment.save()
+        for attachment in tqdm(AttachmentFile.objects.all(), desc='Updating attachments'):
+            attachment.views_count = Event.objects.filter(name='attachment-view', data__id=attachment.id).count()
+            attachment.downloads_count = Event.objects.filter(
+                name='attachment-download', data__id=attachment.id
+            ).count()
+            attachment.save()
