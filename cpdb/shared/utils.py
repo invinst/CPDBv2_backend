c48c0171a06fbbf4d2bedb54573ab40b25c9fab7
ea-open-source <open-source@eastagile.com>
2019-03-04 17:22:55 +0700

    [r] 552 - Make coverage 100%
    [#4236552] https://www.pivotaltracker.com/epic/show/4236552

--- cpdb/shared/utils.py
+++ cpdb/shared/utils.py
@@ -1,13 +1,13 @@
 import time
 
 
-def timing(func):
+def timing(func):  # pragma: no cover
     def return_func(*args, **kwargs):
         return timeit(lambda: func(*args, **kwargs))
     return return_func
 
 
-def timeit(func, start_message=None):
+def timeit(func, start_message=None):  # pragma: no cover
     if start_message:
         print(start_message)
     start_time = time.time()
