460646ff0b148ed2cbdf2872167dfc868dd76996
ea-open-source <open-source@eastagile.com>
2019-03-14 17:02:43 +0700

    [f] 860 - Default pinboard title is empty string
    [#164489860] https://www.pivotaltracker.com/story/show/164489860

--- cpdb/pinboard/models.py
+++ cpdb/pinboard/models.py
@@ -4,7 +4,7 @@ from data.models.common import TimeStampsModel
 
 
 class Pinboard(TimeStampsModel):
-    title = models.CharField(max_length=255, default='Untitled Pinboard')
+    title = models.CharField(max_length=255, default='')
     officers = models.ManyToManyField('data.Officer')
     allegations = models.ManyToManyField('data.Allegation')
     description = models.TextField(blank=True)
