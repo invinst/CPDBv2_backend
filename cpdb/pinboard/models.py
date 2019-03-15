a767d2757096c0f80b09ccd2bf3327799e0d0c2d
ea-open-source <open-source@eastagile.com>
2019-03-15 09:27:46 +0700

    [f] 860 - Allow blank is True in pinboard's title and description
    [#164489860] https://www.pivotaltracker.com/story/show/164489860

--- cpdb/pinboard/models.py
+++ cpdb/pinboard/models.py
@@ -4,7 +4,7 @@ from data.models.common import TimeStampsModel
 
 
 class Pinboard(TimeStampsModel):
-    title = models.CharField(max_length=255, default='')
+    title = models.CharField(max_length=255, default='', blank=True)
     officers = models.ManyToManyField('data.Officer')
     allegations = models.ManyToManyField('data.Allegation')
-    description = models.TextField(blank=True)
+    description = models.TextField(default='', blank=True)
