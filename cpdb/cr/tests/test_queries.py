7250c99ddb42c36948e14c229f1bff4ce7c7cdbc
ea-open-source <open-source@eastagile.com>
2019-04-04 10:58:10 +0700

    [f] 793 - Reorder attachments in New Documents row
    [#164919793] (https://www.pivotaltracker.com/story/show/164919793)

--- cpdb/cr/tests/test_queries.py
+++ cpdb/cr/tests/test_queries.py
@@ -1,8 +1,7 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import pytz
 from django.test.testcases import TestCase
-from django.utils.timezone import now
 
 from robber.expect import expect
 from freezegun import freeze_time
@@ -16,8 +15,8 @@ from cr.queries import LatestDocumentsQuery
 
 
 class LatestDocumentsQueryTestCase(TestCase):
+    @freeze_time('2019-04-03')
     def test_execute(self):
-        three_month_ago = now() - timedelta(weeks=12)
         allegation_1 = AllegationFactory(crid='123')
         allegation_2 = AllegationFactory(crid='456')
         allegation_3 = AllegationFactory(crid='789')
@@ -39,7 +38,7 @@ class LatestDocumentsQueryTestCase(TestCase):
             url='http://cr-document.com/1',
             file_type=MEDIA_TYPE_DOCUMENT,
             preview_image_url='http://preview.com/url1',
-            external_created_at=three_month_ago + timedelta(days=10)
+            external_created_at=datetime(2019, 1, 19, 12, 1, 1, tzinfo=pytz.utc)
         )
         AttachmentFileFactory(
             allegation=allegation_1,
@@ -48,7 +47,7 @@ class LatestDocumentsQueryTestCase(TestCase):
             tag='CR',
             url='http://cr-document.com/2',
             file_type=MEDIA_TYPE_DOCUMENT,
-            external_created_at=three_month_ago + timedelta(days=5)
+            external_created_at=datetime(2019, 1, 14, 10, 12, 1, tzinfo=pytz.utc)
         )
 
         attachment_file_2 = AttachmentFileFactory(
@@ -59,7 +58,7 @@ class LatestDocumentsQueryTestCase(TestCase):
             url='http://cr-document.com/3',
             file_type=MEDIA_TYPE_DOCUMENT,
             preview_image_url='http://preview.com/url3',
-            external_created_at=three_month_ago + timedelta(days=6)
+            external_created_at=datetime(2019, 1, 15, 9, 3, 1, tzinfo=pytz.utc)
         )
 
         AttachmentFileFactory(
@@ -70,19 +69,20 @@ class LatestDocumentsQueryTestCase(TestCase):
             url='http://cr-document.com/4',
             file_type=MEDIA_TYPE_DOCUMENT,
             preview_image_url='http://preview.com/url4',
-            external_created_at=three_month_ago + timedelta(days=10)
+            external_created_at=datetime(2019, 1, 19, 17, 12, 5, tzinfo=pytz.utc)
         )
 
-        AttachmentFileFactory(
-            allegation=allegation_2,
-            title='CR document 5',
-            id=5,
-            tag='AR',
-            url='http://cr-document.com/5',
-            file_type=MEDIA_TYPE_DOCUMENT,
-            preview_image_url='http://preview.com/url5',
-            external_created_at=three_month_ago + timedelta(days=11)
-        )
+        with freeze_time(datetime(2019, 1, 20, 13, 2, 15, tzinfo=pytz.utc)):
+            AttachmentFileFactory(
+                allegation=allegation_2,
+                title='CR document 5',
+                id=5,
+                tag='AR',
+                url='http://cr-document.com/5',
+                file_type=MEDIA_TYPE_DOCUMENT,
+                preview_image_url='http://preview.com/url5',
+                external_created_at=None
+            )
 
         AttachmentFileFactory(
             allegation=allegation_3,
@@ -92,7 +92,7 @@ class LatestDocumentsQueryTestCase(TestCase):
             url='http://cr-document.com/6',
             file_type=MEDIA_TYPE_AUDIO,
             preview_image_url='http://preview.com/url6',
-            external_created_at=three_month_ago + timedelta(days=12)
+            external_created_at=datetime(2019, 1, 21, 6, 4, 12, tzinfo=pytz.utc)
         )
 
         AttachmentFileFactory(
@@ -103,7 +103,7 @@ class LatestDocumentsQueryTestCase(TestCase):
             url='http://cr-document.com/7',
             file_type=MEDIA_TYPE_VIDEO,
             preview_image_url='http://preview.com/url7',
-            external_created_at=three_month_ago + timedelta(days=13)
+            external_created_at=datetime(2019, 1, 22, 4, 9, 12, tzinfo=pytz.utc)
         )
 
         attachment_file_3 = AttachmentFileFactory(
@@ -159,7 +159,7 @@ class LatestDocumentsQueryTestCase(TestCase):
             url='http://cr-document.com/12',
             file_type=MEDIA_TYPE_DOCUMENT,
             preview_image_url='http://preview.com/url12',
-            external_created_at=three_month_ago + timedelta(days=15),
+            external_created_at=datetime(2015, 9, 14, 12, 0, 1, tzinfo=pytz.utc),
             show=False
         )
 
@@ -174,17 +174,16 @@ class LatestDocumentsQueryTestCase(TestCase):
             allegation=allegation_4,
             external_created_at=datetime(2015, 7, 13, 12, 0, 1, tzinfo=pytz.utc)
         )
-
-        with freeze_time(datetime(2018, 8, 14, 12, 0, 1, tzinfo=pytz.utc)):
+        with freeze_time(datetime(2019, 1, 17, 12, 0, 1, tzinfo=pytz.utc)):
             AttachmentTrackingFactory(attachment_file=attachment_file_3)
 
-        with freeze_time(datetime(2018, 9, 14, 12, 0, 1, tzinfo=pytz.utc)):
+        with freeze_time(datetime(2019, 1, 18, 12, 0, 1, tzinfo=pytz.utc)):
             AttachmentTrackingFactory(attachment_file=attachment_file_4)
 
-        with freeze_time(datetime(2018, 7, 14, 12, 0, 1, tzinfo=pytz.utc)):
+        with freeze_time(datetime(2019, 1, 12, 12, 0, 1, tzinfo=pytz.utc)):
             AttachmentTrackingFactory(attachment_file=attachment_file_5)
 
-        with freeze_time(datetime(2018, 10, 14, 12, 0, 1, tzinfo=pytz.utc)):
+        with freeze_time(datetime(2019, 10, 14, 12, 0, 1, tzinfo=pytz.utc)):
             AttachmentTrackingFactory(
                 attachment_file=attachment_file_6,
                 kind=constants.DOWNLOAD_EVENT_TYPE)
@@ -193,143 +192,32 @@ class LatestDocumentsQueryTestCase(TestCase):
 
         results = LatestDocumentsQuery.execute(5)
 
-        expect(len(results)).to.eq(4)
+        expected_results = [
+            {
+                'id': attachment_file_1.id,
+                'allegation_id': '123',
+                'last_active_at': datetime(2019, 1, 19, 12, 1, 1, tzinfo=pytz.utc),
+            },
+            {
+                'id': attachment_file_4.id,
+                'allegation_id': '321',
+                'last_active_at': datetime(2019, 1, 18, 12, 0, 1, tzinfo=pytz.utc),
+            },
+            {
+                'id': attachment_file_2.id,
+                'allegation_id': '456',
+                'last_active_at': datetime(2019, 1, 15, 9, 3, 1, tzinfo=pytz.utc),
+            },
+            {
+                'id': attachment_file_5.id,
+                'allegation_id': '987',
+                'last_active_at': datetime(2019, 1, 12, 12, 0, 1, tzinfo=pytz.utc),
+            },
+        ]
 
-        first_result = results[0]
-        expect(first_result.id).to.eq(attachment_file_4.id)
-        expect(first_result.allegation_id).to.eq('321')
-        expect(first_result.latest_viewed_at).to.eq(datetime(2018, 9, 14, 12, 0, 1, tzinfo=pytz.utc))
-        expect(first_result.num_recent_documents).to.eq(1)
-
-        second_result = results[1]
-        expect(second_result.id).to.eq(attachment_file_5.id)
-        expect(second_result.allegation_id).to.eq('987')
-        expect(second_result.latest_viewed_at).to.eq(datetime(2018, 7, 14, 12, 0, 1, tzinfo=pytz.utc))
-        expect(second_result.num_recent_documents).to.eq(1)
-
-        third_result = results[2]
-        expect(third_result.id).to.eq(attachment_file_1.id)
-        expect(third_result.allegation_id).to.eq('123')
-        expect(third_result.latest_viewed_at).to.be.none()
-        expect(third_result.num_recent_documents).to.eq(2)
-        expect(third_result.allegation.most_common_category_id).to.eq(1)
-
-        fourth_result = results[3]
-        expect(fourth_result.id).to.eq(attachment_file_2.id)
-        expect(fourth_result.allegation_id).to.eq('456')
-        expect(fourth_result.latest_viewed_at).to.be.none()
-        expect(fourth_result.num_recent_documents).to.eq(1)
-
-    def test_execute_with_no_latest_viewed(self):
-        three_month_ago = now() - timedelta(weeks=12)
-        allegation_1 = AllegationFactory(crid='123')
-        allegation_2 = AllegationFactory(crid='456')
-        allegation_3 = AllegationFactory(crid='789')
-
-        attachment_file_1 = AttachmentFileFactory(
-            allegation=allegation_1,
-            title='CR document 1',
-            id=1,
-            tag='CR',
-            url='http://cr-document.com/1',
-            file_type=MEDIA_TYPE_DOCUMENT,
-            preview_image_url='http://preview.com/url1',
-            external_created_at=three_month_ago + timedelta(days=10)
-        )
-        AttachmentFileFactory(
-            allegation=allegation_1,
-            title='CR document 2',
-            id=2,
-            tag='CR',
-            url='http://cr-document.com/2',
-            file_type=MEDIA_TYPE_DOCUMENT,
-            external_created_at=three_month_ago + timedelta(days=5)
-        )
-
-        attachment_file_2 = AttachmentFileFactory(
-            allegation=allegation_2,
-            title='CR document 3',
-            id=3,
-            tag='CR',
-            url='http://cr-document.com/3',
-            file_type=MEDIA_TYPE_DOCUMENT,
-            preview_image_url='http://preview.com/url3',
-            external_created_at=three_month_ago + timedelta(days=6)
-        )
-
-        AttachmentFileFactory(
-            allegation=allegation_2,
-            title='CR document 4',
-            id=4,
-            tag='OCIR',
-            url='http://cr-document.com/4',
-            file_type=MEDIA_TYPE_DOCUMENT,
-            preview_image_url='http://preview.com/url4',
-            external_created_at=three_month_ago + timedelta(days=10)
-        )
-
-        AttachmentFileFactory(
-            allegation=allegation_2,
-            title='CR document 5',
-            id=5,
-            tag='AR',
-            url='http://cr-document.com/5',
-            file_type=MEDIA_TYPE_DOCUMENT,
-            preview_image_url='http://preview.com/url5',
-            external_created_at=three_month_ago + timedelta(days=11)
-        )
-
-        attachment_file_3 = AttachmentFileFactory(
-            allegation=allegation_3,
-            title='CR document 6',
-            id=6,
-            tag='CR',
-            url='http://cr-document.com/6',
-            file_type=MEDIA_TYPE_DOCUMENT,
-            preview_image_url='http://preview.com/url6',
-            external_created_at=three_month_ago + timedelta(days=12)
-        )
-
-        AttachmentFileFactory(
-            allegation=allegation_3,
-            title='CR document 7',
-            id=7,
-            tag='CR',
-            url='http://cr-document.com/7',
-            file_type=MEDIA_TYPE_AUDIO,
-            preview_image_url='http://preview.com/url7',
-            external_created_at=three_month_ago + timedelta(days=13)
-        )
-
-        AttachmentFileFactory(
-            allegation=allegation_3,
-            title='CR document 8',
-            id=8,
-            tag='CR',
-            url='http://cr-document.com/8',
-            file_type=MEDIA_TYPE_VIDEO,
-            preview_image_url='http://preview.com/url8',
-            external_created_at=three_month_ago + timedelta(days=14)
-        )
-
-        results = LatestDocumentsQuery.execute(5)
-
-        expect(len(results)).to.eq(3)
-
-        first_result = results[0]
-        expect(first_result.id).to.eq(attachment_file_3.id)
-        expect(first_result.allegation_id).to.eq('789')
-        expect(first_result.latest_viewed_at).to.be.none()
-        expect(first_result.num_recent_documents).to.eq(1)
-
-        second_result = results[1]
-        expect(second_result.id).to.eq(attachment_file_1.id)
-        expect(second_result.allegation_id).to.eq('123')
-        expect(second_result.latest_viewed_at).to.be.none()
-        expect(second_result.num_recent_documents).to.eq(2)
-
-        third_result = results[2]
-        expect(third_result.id).to.eq(attachment_file_2.id)
-        expect(third_result.allegation_id).to.eq('456')
-        expect(third_result.latest_viewed_at).to.be.none()
-        expect(third_result.num_recent_documents).to.eq(1)
+        expect(len(results)).to.eq(len(expected_results))
+        for index, attachment_data in enumerate(results):
+            expected_attachment_data = expected_results[index]
+            expect(attachment_data.id).to.eq(expected_attachment_data['id'])
+            expect(attachment_data.allegation_id).to.eq(expected_attachment_data['allegation_id'])
+            expect(attachment_data.last_active_at).to.eq(expected_attachment_data['last_active_at'])
