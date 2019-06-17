8da33cb8649a984462c45fe76cc3755f11f0a120
ea-open-source <open-source@eastagile.com>
2019-06-17 15:19:10 +0700

    [f] 776 - Update API CR preview pane of social graph timeline
    [#166658776] (https://www.pivotaltracker.com/story/show/166658776)

--- cpdb/social_graph/serializers.py
+++ cpdb/social_graph/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from data.models import PoliceUnit
 from shared.serializer import NoNullSerializer
+from pinboard.serializers.victim_serializer import VictimSerializer
 
 
 class OfficerPercentileSerializer(NoNullSerializer):
@@ -67,15 +68,74 @@ class AttachmentFileSerializer(NoNullSerializer):
     id = serializers.CharField()
 
 
+class OfficerAllegationPercentileSerializer(serializers.Serializer):
+    percentile_trr = serializers.DecimalField(
+        source='officer.trr_percentile', allow_null=True, read_only=True, max_digits=6, decimal_places=4)
+    percentile_allegation = serializers.DecimalField(
+        source='officer.complaint_percentile', allow_null=True, read_only=True, max_digits=6, decimal_places=4)
+    percentile_allegation_civilian = serializers.DecimalField(
+        source='officer.civilian_allegation_percentile', allow_null=True, read_only=True, max_digits=6, decimal_places=4
+    )
+    percentile_allegation_internal = serializers.DecimalField(
+        source='officer.internal_allegation_percentile', allow_null=True, read_only=True, max_digits=6, decimal_places=4
+    )
+
+
+class CoaccusedSerializer(NoNullSerializer):
+    id = serializers.IntegerField(source='officer.id')
+    full_name = serializers.CharField(source='officer.full_name')
+    complaint_count = serializers.IntegerField(source='officer.allegation_count')
+    sustained_count = serializers.IntegerField(source='officer.sustained_count')
+    birth_year = serializers.IntegerField(source='officer.birth_year')
+    complaint_percentile = serializers.FloatField(
+        read_only=True, allow_null=True, source='officer.complaint_percentile'
+    )
+    recommended_outcome = serializers.CharField(source='recc_outcome')
+    final_outcome = serializers.CharField()
+    final_finding = serializers.CharField(source='final_finding_display')
+    category = serializers.CharField()
+    disciplined = serializers.NullBooleanField()
+    race = serializers.CharField(source='officer.race')
+    gender = serializers.CharField(source='officer.gender_display')
+    rank = serializers.CharField(source='officer.rank')
+    percentile = serializers.SerializerMethodField()
+
+    def get_percentile(self, obj):
+        return OfficerAllegationPercentileSerializer(obj).data
+
+
 class AllegationSerializer(NoNullSerializer):
+    kind = serializers.SerializerMethodField()
     crid = serializers.CharField()
+    to = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    subcategory = serializers.SerializerMethodField()
     incident_date = serializers.DateTimeField(format='%Y-%m-%d')
-    most_common_category = AllegationCategorySerializer()
+    address = serializers.CharField()
+    victims = VictimSerializer(many=True)
+    coaccused = serializers.SerializerMethodField()
     attachments = AttachmentFileSerializer(source='prefetch_filtered_attachment_files', many=True)
     officer_ids = serializers.SerializerMethodField()
 
     def get_officer_ids(self, obj):
         return [officer_allegation.officer_id for officer_allegation in obj.officer_allegations]
+
+    def get_kind(self, obj):
+        return 'CR'
+
+    def get_to(self, obj):
+        return f'/complaint/{obj.crid}/'
+
+    def get_category(self, obj):
+        return obj.most_common_category.category if obj.most_common_category else 'Unknown'
+
+    def get_subcategory(self, obj):
+        return obj.most_common_category.allegation_name if obj.most_common_category else 'Unknown'
+
+    def get_coaccused(self, obj):
+        officer_allegations = obj.officer_allegations
+
+        return CoaccusedSerializer(officer_allegations, many=True).data
 
 
 class AccussedSerializer(NoNullSerializer):
