b35df58d37ef2efb15ee218677957d96a14a234b
ea-open-source <open-source@eastagile.com>
2019-10-16 12:21:20 +0700

    [b] 038 - Use https in sitemap instead of http
    [164625038] https://www.pivotaltracker.com/story/show/164625038

--- cpdb/sitemap/tests/sitemaps/test_static_page_sitemap.py
+++ cpdb/sitemap/tests/sitemaps/test_static_page_sitemap.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
-from sitemap.sitemaps.static_page_sitemap import StaticPageSiteMap
-
 from robber import expect
+
+from sitemap.sitemaps.base_sitemap import BaseSitemap
+from sitemap.sitemaps.static_page_sitemap import StaticPageSiteMap
 
 
 class StaticPageSiteMapTestCase(TestCase):
-    def test_class_properties(self):
-        expect(StaticPageSiteMap.changefreq).to.eq('daily')
-        expect(StaticPageSiteMap.priority).to.eq(0.5)
+    def test_base(self):
+        expect(issubclass(StaticPageSiteMap, BaseSitemap)).to.be.true()
 
     def test_items(self):
         expect(StaticPageSiteMap().items()).to.eq(['', 'collaborate', 'terms'])
