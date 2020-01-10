5b6b99d8bc4935310975442cb4d1cc9e4b2b3f91
ea-open-source <open-source@eastagile.com>
2020-01-10 17:21:38 +0700

    [f] 316 - Remove newline in migration
    [#170374316] https://www.pivotaltracker.com/story/show/170374316

--- cpdb/toast/migrations/0002_create_initial_toast_template.py
+++ cpdb/toast/migrations/0002_create_initial_toast_template.py
@@ -8,22 +8,22 @@ def create_initial_toast_template(apps, schema_editor):
     Toast.objects.get_or_create(
         name='OFFICER',
         defaults={
-            'template': '**{rank} {full_name}** {age} {race} {gender},'
-                        '\nwith *{complaint_count} complaints*, *{sustained_count} sustained* {action_type}.',
+            'template': '**{rank} {full_name}** {age} {race} {gender}, '
+                        'with *{complaint_count} complaints*, *{sustained_count} sustained* {action_type}.',
             'tags': '{rank} {full_name} {birth_year} {gender} {race} {complaint_count} {sustained_count} {action_type}'
         }
     )
     Toast.objects.get_or_create(
         name='CR',
         defaults={
-            'template': '**CR #{crid}** *categorized as {category}*\nhappened in {incident_date} {action_type}.',
+            'template': '**CR #{crid}** *categorized as {category}* happened in {incident_date} {action_type}.',
             'tags': '{crid} {category} {incident_date} {action_type}'
         }
     )
     Toast.objects.get_or_create(
         name='TRR',
         defaults={
-            'template': '**TRR #{id}** *categorized as {force_type}*\nhappened in {incident_date} {action_type}.',
+            'template': '**TRR #{id}** *categorized as {force_type}* happened in {incident_date} {action_type}.',
             'tags': '{id} {force_type} {incident_date} {action_type}'
         }
     )
