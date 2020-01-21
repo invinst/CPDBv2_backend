859f885fcc0fd4af6a7669f28105a21f8874a3ef
ea-open-source <open-source@eastagile.com>
2020-01-21 11:47:07 +0700

    [f] 137 - Search by pinboard title or description text in pinboard admin page
    [#170773137] (https://www.pivotaltracker.com/story/show/170773137)

--- cpdb/pinboard/tests/views/test_desktop_views.py
+++ cpdb/pinboard/tests/views/test_desktop_views.py
@@ -2511,3 +2511,228 @@ class PinboardDesktopViewSetTestCase(APITestCase):
             'allegations': [],
             'trrs': [],
         })
+
+    def test_all_return_pinboard_match_title(self):
+        with freeze_time(datetime(2018, 4, 3, 12, 0, 10, tzinfo=pytz.utc)):
+            officer_1 = OfficerFactory(
+                first_name='Jerome',
+                last_name='Finnigan',
+                allegation_count=0,
+                complaint_percentile='0.0000',
+                trr_percentile='0.0000',
+                civilian_allegation_percentile='0.0000',
+                internal_allegation_percentile='0.0000',
+            )
+            officer_2 = OfficerFactory(
+                first_name='Joe',
+                last_name='Parker',
+                allegation_count=5,
+                complaint_percentile='50.0000',
+                trr_percentile='50.0000',
+                civilian_allegation_percentile='50.0000',
+                internal_allegation_percentile='50.0000',
+            )
+            officer_3 = OfficerFactory(
+                first_name='John',
+                last_name='Hurley',
+                allegation_count=10,
+                complaint_percentile='99.9999',
+                trr_percentile='99.9999',
+                civilian_allegation_percentile='99.9999',
+                internal_allegation_percentile='99.9999',
+            )
+            allegation_1 = AllegationFactory(
+                crid='111111',
+                most_common_category=AllegationCategoryFactory(category='Use Of Force'),
+                incident_date=datetime(2001, 1, 1, tzinfo=pytz.utc),
+            )
+            allegation_2 = AllegationFactory(
+                crid='222222',
+                incident_date=datetime(2002, 2, 2, tzinfo=pytz.utc),
+            )
+            trr_1 = TRRFactory(
+                id='111',
+                trr_datetime=datetime(2001, 1, 1, tzinfo=pytz.utc)
+            )
+            ActionResponseFactory(trr=trr_1, force_type='Use Of Force')
+            trr_2 = TRRFactory(
+                id='222',
+                trr_datetime=datetime(2002, 2, 2, tzinfo=pytz.utc)
+            )
+            PinboardFactory(
+                id='aaaa1111',
+                title='Pinboard Title 1',
+                description='Pinboard description 1',
+                officers=[officer_1, officer_2, officer_3],
+                allegations=[allegation_1, allegation_2],
+                trrs=[trr_1, trr_2],
+            )
+
+        with freeze_time(datetime(2018, 4, 2, 12, 0, 10, tzinfo=pytz.utc)):
+            PinboardFactory(
+                id='bbbb2222',
+                title='Pinboard 2',
+                description='Pinboard description 2',
+            )
+
+        base_url = reverse('api-v2:pinboards-all')
+        admin_user = AdminUserFactory()
+        token, _ = Token.objects.get_or_create(user=admin_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        response = self.client.get(f"{base_url}?match=Title")
+        expect(response.status_code).to.eq(status.HTTP_200_OK)
+        expect(response.data['results']).to.have.length(1)
+
+        expect(response.data['results'][0]).to.eq({
+            'id': 'aaaa1111',
+            'title': 'Pinboard Title 1',
+            'description': 'Pinboard description 1',
+            'created_at': '2018-04-03T12:00:10Z',
+            'officers_count': 3,
+            'allegations_count': 2,
+            'trrs_count': 2,
+            'child_pinboard_count': 0,
+            'officers': [
+                {
+                    'id': officer_3.id,
+                    'name': 'John Hurley',
+                    'count': 10,
+                    'percentile_allegation': '99.9999',
+                    'percentile_trr': '99.9999',
+                    'percentile_allegation_civilian': '99.9999',
+                    'percentile_allegation_internal': '99.9999',
+                    'year': 2016,
+                },
+                {
+                    'id': officer_2.id,
+                    'name': 'Joe Parker',
+                    'count': 5,
+                    'percentile_allegation': '50.0000',
+                    'percentile_trr': '50.0000',
+                    'percentile_allegation_civilian': '50.0000',
+                    'percentile_allegation_internal': '50.0000',
+                    'year': 2016,
+                },
+                {
+                    'id': officer_1.id,
+                    'name': 'Jerome Finnigan',
+                    'count': 0,
+                    'percentile_allegation': '0.0000',
+                    'percentile_trr': '0.0000',
+                    'percentile_allegation_civilian': '0.0000',
+                    'percentile_allegation_internal': '0.0000',
+                    'year': 2016,
+                },
+            ],
+            'allegations': [
+                {
+                    'crid': '222222',
+                    'category': 'Unknown',
+                    'incident_date': '2002-02-02',
+                },
+                {
+                    'crid': '111111',
+                    'category': 'Use Of Force',
+                    'incident_date': '2001-01-01',
+                },
+            ],
+            'trrs': [
+                {
+                    'id': 222,
+                    'trr_datetime': '2002-02-02',
+                    'category': 'Unknown',
+                },
+                {
+                    'id': 111,
+                    'trr_datetime': '2001-01-01',
+                    'category': 'Use Of Force',
+                }
+            ],
+        })
+
+    def test_all_return_pinboard_match_description(self):
+        with freeze_time(datetime(2018, 4, 3, 12, 0, 10, tzinfo=pytz.utc)):
+            officer_1 = OfficerFactory(
+                first_name='Jerome',
+                last_name='Finnigan',
+                allegation_count=0,
+                complaint_percentile='0.0000',
+                trr_percentile='0.0000',
+                civilian_allegation_percentile='0.0000',
+                internal_allegation_percentile='0.0000',
+            )
+            officer_2 = OfficerFactory(
+                first_name='Joe',
+                last_name='Parker',
+                allegation_count=5,
+                complaint_percentile='50.0000',
+                trr_percentile='50.0000',
+                civilian_allegation_percentile='50.0000',
+                internal_allegation_percentile='50.0000',
+            )
+            officer_3 = OfficerFactory(
+                first_name='John',
+                last_name='Hurley',
+                allegation_count=10,
+                complaint_percentile='99.9999',
+                trr_percentile='99.9999',
+                civilian_allegation_percentile='99.9999',
+                internal_allegation_percentile='99.9999',
+            )
+            allegation_1 = AllegationFactory(
+                crid='111111',
+                most_common_category=AllegationCategoryFactory(category='Use Of Force'),
+                incident_date=datetime(2001, 1, 1, tzinfo=pytz.utc),
+            )
+            allegation_2 = AllegationFactory(
+                crid='222222',
+                incident_date=datetime(2002, 2, 2, tzinfo=pytz.utc),
+            )
+            trr_1 = TRRFactory(
+                id='111',
+                trr_datetime=datetime(2001, 1, 1, tzinfo=pytz.utc)
+            )
+            ActionResponseFactory(trr=trr_1, force_type='Use Of Force')
+            trr_2 = TRRFactory(
+                id='222',
+                trr_datetime=datetime(2002, 2, 2, tzinfo=pytz.utc)
+            )
+            PinboardFactory(
+                id='aaaa1111',
+                title='Pinboard Title 1',
+                description='Pinboard description 1',
+                officers=[officer_1, officer_2, officer_3],
+                allegations=[allegation_1, allegation_2],
+                trrs=[trr_1, trr_2],
+            )
+
+        with freeze_time(datetime(2018, 4, 2, 12, 0, 10, tzinfo=pytz.utc)):
+            PinboardFactory(
+                id='bbbb2222',
+                title='Pinboard 2',
+                description='Pinboard description 2',
+            )
+
+        base_url = reverse('api-v2:pinboards-all')
+        admin_user = AdminUserFactory()
+        token, _ = Token.objects.get_or_create(user=admin_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        response = self.client.get(f"{base_url}?match=Pinboard description 2")
+        expect(response.status_code).to.eq(status.HTTP_200_OK)
+        expect(response.data['results']).to.have.length(1)
+
+        expect(response.data['results'][0]).to.eq({
+            'id': 'bbbb2222',
+            'title': 'Pinboard 2',
+            'description': 'Pinboard description 2',
+            'created_at': '2018-04-02T12:00:10Z',
+            'officers_count': 0,
+            'allegations_count': 0,
+            'trrs_count': 0,
+            'child_pinboard_count': 0,
+            'officers': [],
+            'allegations': [],
+            'trrs': [],
+        })
--- cpdb/pinboard/views.py
+++ cpdb/pinboard/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.db.models import Prefetch, Count
+from django.db.models import Q
 
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
@@ -192,6 +193,8 @@ class PinboardDesktopViewSet(PinboardViewSet):
 
     @action(detail=False, methods=['get'])
     def all(self, request):
+        match = request.query_params['match'] if 'match' in request.query_params else ''
+
         if request.user.is_authenticated:
             pinboards = Pinboard.objects.order_by('-created_at').annotate(
                 child_pinboard_count=Count('child_pinboards', distinct=True)
@@ -204,7 +207,7 @@ class PinboardDesktopViewSet(PinboardViewSet):
                         person='Member Action'
                     ).order_by('-action_sub_category', 'force_type')
                 )
-            )
+            ).filter(Q(title__icontains=match) | Q(description__icontains=match))
         else:
             pinboards = []
 
