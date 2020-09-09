b6315d52dadcfd1653acdd67025524a1d53c3419
ea-open-source <open-source@eastagile.com>
2020-09-09 10:19:56 +0700

    [r] Refactor search analyzers

--- cpdb/tracker/doc_types.py
+++ cpdb/tracker/doc_types.py
@@ -1,7 +1,7 @@
 from elasticsearch_dsl import Integer, DocType, Text, Boolean
 
 from .index_aliases import tracker_index_alias
-from search.analyzers import autocomplete, autocomplete_search
+from search.analyzers import autocomplete, autocomplete_search, text_analyzer, text_search_analyzer
 
 
 @tracker_index_alias.doc_type
@@ -9,7 +9,7 @@ class AttachmentFileDocType(DocType):
     id = Integer()
     crid = Text(analyzer=autocomplete, search_analyzer=autocomplete_search)
     title = Text(analyzer=autocomplete, search_analyzer=autocomplete_search)
-    text_content = Text(analyzer=autocomplete, search_analyzer=autocomplete_search)
+    text_content = Text(analyzer=text_analyzer, search_analyzer=text_search_analyzer)
     show = Boolean()
 
     class Meta:
