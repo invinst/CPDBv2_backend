2efe6e69648989fdadde27b8c19b99c323449c79
Eli Itah <eli@spantree.net>
2023-06-02 22:05:36 -0500

    Testing if CICD can apply the controller RBAC

--- bin/initialize_kubernetes_cluster.sh
+++ bin/initialize_kubernetes_cluster.sh
@@ -4,8 +4,8 @@ set -e
 if [ "$1" == "-h" -o "$1" == "--help" ]; then
     echo "Initialize kubernetes cluster."
     echo ""
-    echo "Usage: `basename $0` {--production|--beta|--staging}"
-    echo "       `basename $0` {-h|--help}"
+    echo "Usage: $(basename $0) {--production|--beta|--staging}"
+    echo "       $(basename $0) {-h|--help}"
     exit 0
 elif [ "$1" == "--production" ]; then
     ENV_FILE=prod.env
@@ -21,7 +21,7 @@ else
     exit 1
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $DIR/..
 source $ENV_FILE
 export $(cut -d= -f1 $ENV_FILE)
@@ -31,5 +31,6 @@ kubectl apply -f kubernetes/secrets-$NAMESPACE.yml
 
 # Create nginx ingress controller
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.0/deploy/static/provider/cloud/deploy.yaml -n ingress-nginx
