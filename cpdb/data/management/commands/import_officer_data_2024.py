35f961113a3544dc6659e5c3f9c76613c2c03e23
Hector Iturre <dev@79x.solutions>
2024-06-27 18:13:29 -0300

    Adding logic to check if csv is being loaded

--- cpdb/data/management/commands/import_officer_data_2024.py
+++ cpdb/data/management/commands/import_officer_data_2024.py
@@ -23,62 +23,76 @@ class Command(BaseCommand):
         if not file_path:
             logger.error("Please provide a valid file path.")
             return
+        try:
+            with open(file_path) as f:
+                reader = DictReader(f)
 
-        with open(file_path) as f:
-            reader = DictReader(f)
-            tag = ''
-            with transaction.atomic():
-                with connection.constraint_checks_disabled():
-                    cursor = connection.cursor()
-                    print("Dropping constraints")
-                    cursor.execute('SET CONSTRAINTS ALL IMMEDIATE;')
-                    cursor.execute('ALTER TABLE public.data_officer ALTER COLUMN tags DROP NOT NULL;')
-                    print("Deleting previous objects")
-                    cursor.execute('delete from trr_charge where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
-                    cursor.execute('delete from trr_subjectweapon where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
-                    cursor.execute('delete from trr_actionresponse where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
-                    cursor.execute('delete from trr_trrattachmentrequest where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
-                    cursor.execute('delete from trr_trrstatus where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
-                    cursor.execute('delete from trr_weapondischarge where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
-                    cursor.execute('delete from pinboard_pinboard_trrs  where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
-                    cursor.execute('delete from trr_trr where officer_id in (select id from data_officer)')
+                if not reader:
+                    logger.error("The provided CSV file is empty or invalid.")
+                    return
 
-                    Officer.objects.all().delete()
+                tag = ''
+                with transaction.atomic():
+                    with connection.constraint_checks_disabled():
+                        cursor = connection.cursor()
+                        print("Dropping constraints")
+                        cursor.execute('SET CONSTRAINTS ALL IMMEDIATE;')
+                        cursor.execute('ALTER TABLE public.data_officer ALTER COLUMN tags DROP NOT NULL;')
+                        print("Deleting previous objects")
+                        cursor.execute('delete from trr_charge where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
+                        cursor.execute('delete from trr_subjectweapon where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
+                        cursor.execute('delete from trr_actionresponse where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
+                        cursor.execute('delete from trr_trrattachmentrequest where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
+                        cursor.execute('delete from trr_trrstatus where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
+                        cursor.execute('delete from trr_weapondischarge where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
+                        cursor.execute('delete from pinboard_pinboard_trrs  where trr_id in (select id from trr_trr where officer_id in (select id from data_officer) )')
+                        cursor.execute('delete from trr_trr where officer_id in (select id from data_officer)')
 
-                    for row in tqdm(reader, desc='Updating officers'):
-                        officer = Officer(id=row['UID'])
+                        Officer.objects.all().delete()
 
-                        officer.last_name = row['last_name'].strip()
-                        officer.first_name = row['first_name'].strip()
-                        officer.middle_initial = row['middle_initial'].strip()
-                        officer.middle_initial2 = row['middle_initial2'].strip()
-                        officer.suffix_name = row['suffix_name'].strip()
-                        if row['birth_year'].strip() == '':
-                            officer.birth_year = None
-                        else:
-                            officer.birth_year = row['birth_year'].strip()
-                        officer.race = row['race'].strip()
-                        officer.gender = row['gender'].strip()[0] if row['gender'].strip() else ''
-                        officer.appointed_date = datetime.strptime(row['appointed_date'], '%Y-%m-%d') if row[
-                            'appointed_date'].strip() else None
-                        officer.resignation_date = datetime.strptime(row['resignation_date'], '%Y-%m-%d') if row[
-                            'resignation_date'].strip() else None
-                        officer.current_status = row['current_status'].strip()
-                        officer.current_star = row['current_star'].strip()
-                        officer.current_unit = row['current_unit'].strip()
-                        officer.current_rank = row['current_rank'].strip()
-                        officer.foia_names = row['foia_names'].strip()
-                        officer.matches = row['matches'].strip()
-                        officer.profile_count = row['profile_count'].strip()
+                        for row in tqdm(reader, desc='Updating officers'):
+                            print("----------------")
+                            print(row)
+                            print("----------------")
+                            uid = row.get('UID')
+                            if uid is None:
+                                logger.warning(f"UID is missing in row: {row}")
+                                continue
+                            officer = Officer(id=row['UID'])
 
-                        if not tag:
-                            tag = officer.tags
-                            logger.info(f"Tag set to: {tag}")
+                            officer.last_name = row['last_name'].strip()
+                            officer.first_name = row['first_name'].strip()
+                            officer.middle_initial = row['middle_initial'].strip()
+                            officer.middle_initial2 = row['middle_initial2'].strip()
+                            officer.suffix_name = row['suffix_name'].strip()
+                            if row['birth_year'].strip() == '':
+                                officer.birth_year = None
+                            else:
+                                officer.birth_year = row['birth_year'].strip()
+                            officer.race = row['race'].strip()
+                            officer.gender = row['gender'].strip()[0] if row['gender'].strip() else ''
+                            officer.appointed_date = datetime.strptime(row['appointed_date'], '%Y-%m-%d') if row[
+                                'appointed_date'].strip() else None
+                            officer.resignation_date = datetime.strptime(row['resignation_date'], '%Y-%m-%d') if row[
+                                'resignation_date'].strip() else None
+                            officer.current_status = row['current_status'].strip()
+                            officer.current_star = row['current_star'].strip()
+                            officer.current_unit = row['current_unit'].strip()
+                            officer.current_rank = row['current_rank'].strip()
+                            officer.foia_names = row['foia_names'].strip()
+                            officer.matches = row['matches'].strip()
+                            officer.profile_count = row['profile_count'].strip()
 
-                        officer.save()
+                            if not tag:
+                                tag = officer.tags
+                                logger.info(f"Tag set to: {tag}")
 
-                    cursor.execute("UPDATE public.data_officer SET tags = '{}' WHERE tags is null;")
-                    cursor.execute('ALTER TABLE public.data_officer ALTER COLUMN tags SET NOT NULL;')
-                    cursor.execute('SET CONSTRAINTS ALL DEFERRED;')
+                            officer.save()
+
+                        cursor.execute("UPDATE public.data_officer SET tags = '{}' WHERE tags is null;")
+                        cursor.execute('ALTER TABLE public.data_officer ALTER COLUMN tags SET NOT NULL;')
+                        cursor.execute('SET CONSTRAINTS ALL DEFERRED;')
+        except Exception as ex:
+            print(ex)
 
         logger.info("Officers Finished successfully")
