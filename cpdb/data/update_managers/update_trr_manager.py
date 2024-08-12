99cebeec111855ce192fc00e6d84b907db5669d2
Ashwin Sharma <ashwindsharma@gmail.com>
2024-08-12 09:45:46 -0500

    fixing trr update script to use regular date when datetime is null. should really add a not null constraint to trr_datetime given that it breaks the timeline if it doesn't exist

--- cpdb/data/update_managers/update_trr_manager.py
+++ cpdb/data/update_managers/update_trr_manager.py
@@ -37,7 +37,7 @@ class UpdateTRRManager(UpdateManagerBase):
                     d.direction,
                     initcap(nullif(street, '')) as street,
                     initcap(nullif(location, '')) as location,
-                    nullif(trr_datetime, '')::timestamp as trr_datetime,
+                    coalesce(nullif(trr_datetime, ''), trr_date)::timestamp as trr_datetime,
                     initcap(nullif(indoor_or_outdoor, '')) as indoor_or_outdoor,
                     nullif(lighting_condition, '') as lighting_condition,
                     nullif(weather_condition, '') as weather_condition,
